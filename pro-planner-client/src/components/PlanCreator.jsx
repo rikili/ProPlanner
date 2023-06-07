01f3befa521f5700e8fb93380406e8b218827746
R <rikli258@gmail.com>
2023-06-07 14:59:21 -0700

    refactor to combine time + date storage

--- pro-planner-client/src/components/PlanCreator.jsx
+++ pro-planner-client/src/components/PlanCreator.jsx
@@ -4,27 +4,46 @@ import { Button } from 'react-bootstrap';
 
 import InputDetailsForm from './InputDetailsForm';
 import TimeRangeForm from './TimeRangeForm';
-import { updatePlan } from '../redux/planParamSlice';
+import { updatePlan, PLAN_TYPE } from '../redux/planParamSlice';
 
 const PlanCreator = ({ title = <>Setup the <b>Plan</b></> }) => { 
     //TODO: currently title is customizable, may want to consider removing
     //      if creator isn't re-used (ie. if we use this comp for editing)
     const detailForm = useRef(null);
     const timeForm = useRef(null);
-    const showTimeForm = useSelector(state => state.planParameters.planType) === 'Outing';
+    const isOuting = useSelector(state => state.planParameters.planType) === PLAN_TYPE.OUTING;
     const dispatch = useDispatch();
 
     const handleFormSubmission = () => {
         const detailResults = detailForm.current.retrieveData();
-        const timeResults = timeForm.current ? timeForm.current.retrieveData() : null;
-        console.log({...detailResults, ...timeResults});
-        if (timeResults) dispatch(updatePlan({...detailResults, isAllDay: true}));
-        else dispatch(updatePlan({...detailResults, ...timeResults}));
+        let formResult = {
+            name: detailResults.name,
+            location: detailResults.location,
+            isAllDay: true,
+            dateTimeRange: [],
+            availableDays: detailResults.availableDays,
+        };
+
+        const timeResults = isOuting ? timeForm.current.retrieveData() : null;
+        if (isOuting && !timeResults.isAllDay) {
+            const startDateTime = new Date(`${detailResults.dateRange[0]}, ${timeResults.timeRange[0]}`);
+            const endTime = new Date(`${detailResults.dateRange[0]}, ${timeResults.timeRange[1]}`);
+            const endDate = new Date(`${detailResults.dateRange[1]}, ${timeResults.timeRange[1]}`);
+
+            formResult.isAllDay = false;
+            formResult.dateTimeRange =  [[startDateTime, endTime], endDate];
+        } else {
+            const startDateTime = new Date(`${detailResults.dateRange[0]}, 00:00:00`);
+            const endDate = new Date(`${detailResults.dateRange[1]}, 23:59:59`);
+            formResult.dateTimeRange = [startDateTime, endDate];
+        }
+        
+        dispatch(updatePlan(formResult));
     }
 
     return <div className="w-50 mx-auto mt-5">
         <InputDetailsForm ref={detailForm} title={title} />
-        {showTimeForm && <TimeRangeForm ref={timeForm} />}
+        {isOuting && <TimeRangeForm ref={timeForm} />}
         <div className="text-center m-3">
             <Button className="w-50" variant="success" size="md" onClick={handleFormSubmission}>
                 <b>Submit</b>
--- pro-planner-client/src/components/TimeRangeForm.jsx
+++ pro-planner-client/src/components/TimeRangeForm.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef, forwardRef, useImperativeHandle } from 'react';
 import { Card, Form, Button, Row, Col } from 'react-bootstrap';
 
-const TimeRangeForm = forwardRef((_, ref) => {
+const roundingSteps = [0, 15, 30, 45, 60];
+
+const TimeRangeForm = forwardRef((props, ref) => {
     const [isAllDay, setAllDay] = useState(false);
     const timeForm = useRef(null);
 
@@ -23,23 +25,46 @@ const TimeRangeForm = forwardRef((_, ref) => {
         }
     });
 
+
+    const handleTimeBlur = (event) => {
+        const strVal = event.target.value;
+        if (strVal) {
+            const timeSplit = strVal.match(/[0-9]+/g);
+            const minVal = Number(timeSplit[1]);
+            const closestStep = roundingSteps.map((step) => Math.abs(step - minVal))
+                .reduce((lowestSoFar, currDiff, index) => {
+                    const diff = lowestSoFar[0];
+                    if (diff >= currDiff) return [currDiff, index];
+                    return lowestSoFar;
+                }, [60, 0]);
+            let newHour = Number(timeSplit[0]);
+            let newMin = roundingSteps[closestStep[1]];
+            const updateHour = newMin === 60;
+            if (updateHour) {
+                newMin = 0;
+                newHour = newHour >= 23 ? 0 : newHour + 1;
+            }
+            event.target.value = `${newHour.toLocaleString('en-US', { minimumIntegerDigits: 2 })}:${newMin.toLocaleString('en-US', { minimumIntegerDigits: 2 })}`;
+        }
+    }
+
     return <Card className="m-3">
         <Card.Body className="ps-5 pe-5">
-            <h6 className="mb-2"><b>Set Date Range</b></h6>
+            <h6 className="mb-4"><b>Set a Time Range</b></h6>
             <Form className="d-flex flex-column align-items-center" ref={timeForm} onSubmit={(e) => e.preventDefault()}>
                 <Row className="mb-2">
                     <Form.Group controlId="formTimeStart" as={Col}>
                         <Form.Label>Start Time </Form.Label>
-                        <Form.Control type='time' className={`${isAllDay ? '' : 'Disabled readonly'}`} disabled={isAllDay} />
+                        <Form.Control type='time' className={`${isAllDay ? '' : 'Disabled readonly'}`} disabled={isAllDay} onBlur={handleTimeBlur} />
                     </Form.Group>
                     <Form.Group controlId="formTimeEnd" as={Col}>
                         <Form.Label>End Time </Form.Label>
-                        <Form.Control type='time' className={`${isAllDay ? '' : 'Disabled readonly'}`} disabled={isAllDay} />
+                        <Form.Control type='time' className={`${isAllDay ? '' : 'Disabled readonly'}`} disabled={isAllDay} onBlur={handleTimeBlur} />
                     </Form.Group>
                 </Row>
                 <div className="w-100">
                     <p className="text-center mb-2">OR</p>
-                    <Button 
+                    <Button
                         className="w-100"
                         variant={`${isAllDay ? 'primary' : 'secondary'}`}
                         active={isAllDay}
