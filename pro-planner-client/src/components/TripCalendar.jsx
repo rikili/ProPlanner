a27ae0330fb8d199c8416a24a816d0768acf3e57
Parham Alijani <palijani@students.cs.ubc.ca>
2023-06-08 01:49:17 -0700

    Implemented handleChangeMonth

--- pro-planner-client/src/components/TripCalendar.jsx
+++ pro-planner-client/src/components/TripCalendar.jsx
@@ -1,9 +1,15 @@
-import './TripCalendar.css'
-import Cell from './Cell.js'
-import { useState, useEffect } from 'react'
-import { format, compareAsc, getDay, startOfMonth, endOfMonth, addMonths, subMonths } from 'date-fns/fp'
-
-
+import './TripCalendar.css';
+import Cell from './Cell.js';
+import { useState, useEffect } from 'react';
+import {
+	format,
+	compareAsc,
+	getDay,
+	startOfMonth,
+	endOfMonth,
+	addMonths,
+	subMonths,
+} from 'date-fns';
 
 // const formMonthMap = (inputDate) => {
 //     const firstDay = new Date(inputDate);
@@ -22,81 +28,105 @@ import { format, compareAsc, getDay, startOfMonth, endOfMonth, addMonths, subMon
 //     return monthArr;
 // }
 const MONTHS = {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December',
-}
+	0: 'January',
+	1: 'February',
+	2: 'March',
+	3: 'April',
+	4: 'May',
+	5: 'June',
+	6: 'July',
+	7: 'August',
+	8: 'September',
+	9: 'October',
+	10: 'November',
+	11: 'December',
+};
 
 const TripCalendar = () => {
-    // temp variables
-    const monthArray = [[1,2,3,4, 5, 6, 7],[5,6,7,8,9,10,11],[5,6,7,8,9,10,11],[5,6,7,8,9,10,11],[5,6,7,8,9,10,11],[5,6,7,8,9,10,11]];
-    const startDate = new Date(2021, 5, 15)
-    const endDate = new Date(2024, 6, 20)
+	// temp variables
+	const monthArray = [
+		[1, 2, 3, 4, 5, 6, 7],
+		[5, 6, 7, 8, 9, 10, 11],
+		[5, 6, 7, 8, 9, 10, 11],
+		[5, 6, 7, 8, 9, 10, 11],
+		[5, 6, 7, 8, 9, 10, 11],
+		[5, 6, 7, 8, 9, 10, 11],
+	];
+	const startDate = new Date(2021, 5, 15);
+	const endDate = new Date(2022, 6, 20);
 
-   
-   
-    const [startDayOfMonth, setStartDayOfMonth] = useState(startOfMonth(startDate));
+	const [startDayOfMonth, setStartDayOfMonth] = useState(
+		startOfMonth(startDate)
+	);
 
-    const [currYear, setCurrYear] = useState(startDate.getFullYear());
-    const [currMonth, setCurrMonth] = useState(Number.parseInt(startDate.getMonth()));
+	const [currYear, setCurrYear] = useState(startDate.getFullYear());
+	const [currMonth, setCurrMonth] = useState(
+		Number.parseInt(startDate.getMonth())
+	);
 
+	// Handle pressing next/previous month.
+	// Once you go next and reach the endDate, you cant go further.
+	// Once you go back and reach the startDate, you cant go further.
+	const handleChangetMonth = isNext => {
+		const isAfter = compareAsc(endDate, addMonths(startDayOfMonth, 1)) == 1;
+		const isBefore = compareAsc(startDate, subMonths(startDayOfMonth, 1)) == -1;
 
-    // Handle pressing next month -> changes the state of currMonth, and if December is our currState, then changes the state of currYear
-    const handleChangetMonth = (isNext) => {
-        if (isNext && startDayOfMonth) {
-            setStartDayOfMonth(addMonths(startDayOfMonth, 1));
-            console.log(startDayOfMonth)
-        } else {
-            setStartDayOfMonth(subMonths(startDayOfMonth, 1));
-            console.log(startDayOfMonth)
-        }
-    }
+		if (isNext && isAfter) {
+			setStartDayOfMonth(addMonths(startDayOfMonth, 1));
+		} else if (!isNext && isBefore) {
+			setStartDayOfMonth(subMonths(startDayOfMonth, 1));
+		}
+	};
 
-        useEffect(() => {
-        console.log(startDayOfMonth);
-    }, [startDayOfMonth]);
+	console.log('UPDATE: ' + startDayOfMonth);
 
-
-    return <div>
-        Trip Calendar!
-        <div className="calendar-grid">
-            <header class="calendar-toolbar">
-                <button onClick={() => handleChangetMonth(false)} style={ {background: 'inherit', border: 'none'} }>{'<'}</button>
-                <p> {currYear + " " + MONTHS[currMonth]}</p>
-                <button onClick={() => handleChangetMonth(true)} style={ {background: 'inherit', border: 'none'}}>{'>'}</button>
-            </header>
-            <main>
-                <ul className='weekday-labels'>
-                    <li>Sun</li>
-                    <li>Mon</li>
-                    <li>Tue</li>
-                    <li>Wed</li>
-                    <li>Thu</li>
-                    <li>Fri</li>
-                    <li>Sat</li>
-                </ul>
-                {monthArray.map((weekArr) => {
-                    return <div className="week-container">
-                        {weekArr.map((day) => {
-                            return <div className="day-container">
-                                <Cell className="half-day" type="AM" date={ startDate } />
-                                <Cell className="half-day" type="PM"  date={ startDate }/>
-                            </div>
-                        })}
-                    </div>
-                })}
-            </main>
-        </div>
-    </div>
-}
+	return (
+		<div>
+			Trip Calendar!
+			<div className="calendar-grid">
+				<header className="calendar-toolbar">
+					<button
+						onClick={() => handleChangetMonth(false)}
+						style={{ background: 'inherit', border: 'none' }}
+					>
+						{'<'}
+					</button>
+					<p> {currYear + ' ' + MONTHS[currMonth]}</p>
+					<button
+						onClick={() => handleChangetMonth(true)}
+						style={{ background: 'inherit', border: 'none' }}
+					>
+						{'>'}
+					</button>
+				</header>
+				<main>
+					<ul className="weekday-labels">
+						<li>Sun</li>
+						<li>Mon</li>
+						<li>Tue</li>
+						<li>Wed</li>
+						<li>Thu</li>
+						<li>Fri</li>
+						<li>Sat</li>
+					</ul>
+					{monthArray.map(weekArr => {
+						return (
+							<div className="week-container">
+								{weekArr.map(day => {
+									return (
+										<div className="day-container">
+											<Cell className="half-day" type="AM" date={startDate} />
+											<Cell className="half-day" type="PM" date={startDate} />
+										</div>
+									);
+								})}
+							</div>
+						);
+					})}
+				</main>
+			</div>
+		</div>
+	);
+};
 
 export default TripCalendar;
