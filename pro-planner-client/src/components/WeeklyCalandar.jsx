b3e9ea7c25f35cd1434fc496b496ac2e31415769
R <rikli258@gmail.com>
2023-06-08 20:21:41 -0700

    reformatting + style touch up

--- pro-planner-client/src/components/WeeklyCalandar.jsx
+++ pro-planner-client/src/components/WeeklyCalandar.jsx
@@ -5,67 +5,63 @@
 // conditional className references https://stackoverflow.com/questions/30533171/react-js-conditionally-applying-class-attributes
 
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { addWeeks, isWithinInterval, addDays, startOfWeek } from 'date-fns';
-import { processDates, getWeekInterval } from '../helpers/Outing';
+import { format } from 'date-fns';
+import { processDates } from '../helpers/Outing';
 import Day from './Day';
 import Hours from './Hours';
 import Button from 'react-bootstrap/Button';
 
 function WeeklyCalandar() {
-  const startTime = new Date('2023-06-03, 01:00:00');
-  const endTime = new Date('2023-06-03, 11:00:00');
-  const endDate = new Date('2023-06-9, 11:00:00');
-  const params = {
-    availableDays: [0, 1, 2, 5, 6],
-    dateTimeRange: [[startTime, endTime], endDate],
-  };
-  const [selectWeek, setSelectWeek] = useState(0);
+    const startTime = new Date('2023-06-03, 01:00:00');
+    const endTime = new Date('2023-06-03, 23:00:00');
+    const endDate = new Date('2023-06-21, 23:00:00');
+    const params = {
+        availableDays: [0, 1, 3, 5, 6],
+        dateTimeRange: [[startTime, endTime], endDate],
+    };
+    const [selectWeek, setSelectWeek] = useState(0);
 
-  let dates = processDates(params);
+    let dates = processDates(params);
 
-  const calendarDisplay = [];
-  let currWeek = [];
-  let weekCount = 0;
-  for (const availability of dates) {
-    currWeek.push(availability);
-    weekCount++;
-    if (weekCount >= 7) {
-      calendarDisplay.push(currWeek);
-      currWeek = [];
-      weekCount = 0;
+    const calendarDisplay = [];
+    let currWeek = [];
+    let weekCount = 0;
+    for (const availability of dates) {
+        currWeek.push(availability);
+        weekCount++;
+        if (weekCount >= 7) {
+            calendarDisplay.push(currWeek);
+            currWeek = [];
+            weekCount = 0;
+        }
     }
-  }
 
-  console.log(calendarDisplay);
-
-  function changeWeek(type) {
-    if (type) {
-      setSelectWeek(selectWeek + 1);
-    } else {
-      setSelectWeek(selectWeek - 1);
+    function changeWeek(type) {
+        if (type) {
+            setSelectWeek(selectWeek + 1);
+        } else {
+            setSelectWeek(selectWeek - 1);
+        }
     }
-  }
 
-  return (
-    <div className="d-flex flex-column w-75 m-auto">
-      <div className="d-flex">
-        <Hours />
-        {calendarDisplay[selectWeek].map((availability, index) => (
-          <Day key={index} availability={availability} />
-        ))}
-      </div>
-      <div className="d-flex justify-content-end">
-        <Button onClick={() => changeWeek(false)} className={selectWeek !== 0 ? '' : 'disabled'}>
-          Prev
-        </Button>
-        <Button onClick={() => changeWeek(true)} className={'next-btn ' + (calendarDisplay.length - 1 > selectWeek ? '' : 'disabled')}>
-          Next
-        </Button>
-      </div>
-    </div>
-  );
-  // let showDays = dates.map((day) => <Day day={day} />);
-  // return <div>{showDays}</div>;
+    return (
+        <div className="d-flex flex-column w-75 m-auto">
+            <h3>{format(calendarDisplay[selectWeek][0].startInterval, 'MMMM')}</h3>
+            <div className="d-flex">
+                <Hours />
+                {calendarDisplay[selectWeek].map((availability, index) => (
+                    <Day key={`day-${index}`} availability={availability} borderLeft={index === 0} />
+                ))}
+            </div>
+            <div className="d-flex justify-content-end">
+                <Button onClick={() => changeWeek(false)} className={selectWeek !== 0 ? '' : 'disabled'}>
+                    Prev
+                </Button>
+                <Button onClick={() => changeWeek(true)} className={'next-btn ' + (calendarDisplay.length - 1 > selectWeek ? '' : 'disabled')}>
+                    Next
+                </Button>
+            </div>
+        </div>
+    );
 }
 export default WeeklyCalandar;
