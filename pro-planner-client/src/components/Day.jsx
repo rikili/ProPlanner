3474cb6a660fdd9291ba14a5e942a80aba4764c0
R <rikli258@gmail.com>
2023-06-08 20:21:41 -0700

    reformatting + style touch up

--- pro-planner-client/src/components/Day.jsx
+++ pro-planner-client/src/components/Day.jsx
@@ -7,51 +7,60 @@ import './Day.css';
 const segmentInterval = 30; // should be some interval of 15 minutes
 
 const isSegmentAvailable = (time, interval, additionalInterval) => {
-  let result = isWithinInterval(time, { start: interval[0], end: addMinutes(interval[1], -1) });
-  if (additionalInterval[0] && !result) {
-    result = isWithinInterval(time, { start: additionalInterval[0], end: additionalInterval[0] });
-  }
-  return result;
+    let result = isWithinInterval(time, { start: interval[0], end: addMinutes(interval[1], -1) });
+    if (additionalInterval[0] && !result) {
+        result = isWithinInterval(time, { start: additionalInterval[0], end: additionalInterval[0] });
+    }
+    return result;
 };
 
-function Day({ availability }) {
-  const segmentTotal = 60;
+function Day({ availability, borderLeft = false }) {
+    let isFirstSegment = true; // For styling top of day
+    const segmentTotal = 60;
 
-  const { startInterval, endInterval, additionalStart, additionalEnd, isAvailable } = availability;
-  let dayDisplay = [];
-  let hourDisplay = [];
-  let segmentTracker = segmentTotal;
-  let timeTracker = startOfDay(startInterval);
-  const endOfCurrDay = endOfDay(startInterval);
+    const { startInterval, endInterval, additionalStart, additionalEnd, isAvailable } = availability;
+    let dayDisplay = [];
+    let hourDisplay = [];
+    let segmentTracker = segmentTotal;
+    let timeTracker = startOfDay(startInterval);
+    const endOfCurrDay = endOfDay(startInterval);
 
-  while (isAfter(endOfCurrDay, timeTracker)) {
-    const isSelectable = isAvailable ? isSegmentAvailable(timeTracker, [startInterval, endInterval], [additionalStart, additionalEnd]) : false;
-    hourDisplay.push(
-      <div
-        key={`${format(timeTracker, 'hh-mm')}`}
-        className={`${segmentTracker % segmentTotal ? 'segment' : 'sub-segment'} ${isSelectable ? 'selected' : ''}`}
-      />
-    );
+    while (isAfter(endOfCurrDay, timeTracker)) {
+        const isSelectable = isAvailable ? isSegmentAvailable(timeTracker, [startInterval, endInterval], [additionalStart, additionalEnd]) : false;
+        const segmentClass = [
+            segmentTracker % segmentTotal && 'segment',
+            !(segmentTracker % segmentTotal) && 'sub-segment',
+            isSelectable && 'selectable',
+            isFirstSegment && 'top-segment',
+            borderLeft && 'left-segment'
+        ].filter(Boolean).join(' ');
+        hourDisplay.push(
+            <div
+                key={`${format(timeTracker, 'hh-mm')}`}
+                className={segmentClass}
+            />
+        );
+        isFirstSegment = false;
 
-    timeTracker = addMinutes(timeTracker, segmentInterval);
-    segmentTracker -= segmentInterval;
-    if (segmentTracker <= 0) {
-      dayDisplay.push(
-        <div key={`hr-${timeTracker.getHours()}-${timeTracker.getMinutes()}`} className="hour-container">
-          {hourDisplay}
-        </div>
-      );
-      hourDisplay = [];
-      segmentTracker = segmentTotal;
+        timeTracker = addMinutes(timeTracker, segmentInterval);
+        segmentTracker -= segmentInterval;
+        if (segmentTracker <= 0) {
+            dayDisplay.push(
+                <div key={`hr-${timeTracker.getHours()}-${timeTracker.getMinutes()}`} className="hour-container">
+                    {hourDisplay}
+                </div>
+            );
+            hourDisplay = [];
+            segmentTracker = segmentTotal;
+        }
     }
-  }
 
-  return (
-    <div className="w-100 position-relative text-center">
-      {getDate(startInterval)}
-      {dayDisplay}
-    </div>
-  );
+    return (
+        <div className="w-100 position-relative text-center">
+            {getDate(startInterval)}
+            {dayDisplay}
+        </div>
+    );
 }
 
 export default Day;
--- pro-planner-client/src/helpers/Outing.js
+++ pro-planner-client/src/helpers/Outing.js
@@ -1,107 +1,107 @@
 // code references https://date-fns.org/docs/Getting-Started
 
-import { isAfter, set, getDay, getHours, getMinutes, addDays, getDate, startOfWeek, endOfWeek, startOfDay, endOfDay } from 'date-fns';
+import { isAfter, set, getDay, getHours, getMinutes, addDays, getDate, startOfWeek, endOfWeek, startOfDay } from 'date-fns';
 
 export function processDates(params) {
-  const startInterval = params.dateTimeRange[0][0];
-  const endInterval = params.dateTimeRange[0][1];
-  const endDate = params.dateTimeRange[1];
+    const startInterval = params.dateTimeRange[0][0];
+    const endInterval = params.dateTimeRange[0][1];
+    const endDate = params.dateTimeRange[1];
 
-  let nextDayOverFlow = false;
-  if (getDate(startInterval) !== getDate(endInterval)) {
-    nextDayOverFlow = true;
-  }
+    let nextDayOverFlow = false;
+    if (getDate(startInterval) !== getDate(endInterval)) {
+        nextDayOverFlow = true;
+    }
 
-  let startWeekForInterval = startOfWeek(startInterval);
+    let startWeekForInterval = startOfWeek(startInterval);
 
-  let datesIndex = 0;
-  let dates = [];
-  while (getDay(startInterval) !== getDay(startWeekForInterval)) {
-    dates[datesIndex] = {
-      isAvailable: false,
-      startInterval: startWeekForInterval,
-    };
-    startWeekForInterval = addDays(startWeekForInterval, 1);
+    let datesIndex = 0;
+    let dates = [];
+    while (getDay(startInterval) !== getDay(startWeekForInterval)) {
+        dates[datesIndex] = {
+            isAvailable: false,
+            startInterval: startWeekForInterval,
+        };
+        startWeekForInterval = addDays(startWeekForInterval, 1);
+        datesIndex++;
+    }
+
+    dates.push({
+        isAvailable: true,
+        startInterval: startInterval,
+        endInterval: endInterval,
+    });
     datesIndex++;
-  }
-
-  dates.push({
-    isAvailable: true,
-    startInterval: startInterval,
-    endInterval: endInterval,
-  });
-  datesIndex++;
-  if (nextDayOverFlow) {
-    dates[0].endInterval = startInterval(startInterval);
-  }
-  let currDate = addDays(startInterval, 1);
-  let tempStartInterval = startInterval;
-  let tempEndInterval = endInterval;
-
-  let dayOfWeekIndex = getDay(startInterval) === 6 ? 0 : getDay(startInterval) + 1;
-
-  let firstDay = true;
-  while (!isAfter(currDate, endDate)) {
-    dates[datesIndex] = {};
-    if (params.availableDays.includes(dayOfWeekIndex)) {
-      dates[datesIndex].isAvailable = true;
-    } else {
-      dates[datesIndex].isAvailable = false;
+    if (nextDayOverFlow) {
+        dates[0].endInterval = startInterval(startInterval);
     }
-    dayOfWeekIndex++;
-    if (dayOfWeekIndex === 7) {
-      dayOfWeekIndex = 0;
-    }
+    let currDate = addDays(startInterval, 1);
+    let tempStartInterval = startInterval;
+    let tempEndInterval = endInterval;
 
-    if (dates[datesIndex].isAvailable) {
-      if (nextDayOverFlow) {
-        if (!firstDay) {
-          let additionalStartInterval = startOfDay(currDate);
-          let additionalEndInterval = set(new Date(), {
-            date: getDate(currDate),
-            hours: getHours(endInterval),
-            minutes: getMinutes(endInterval),
-            seconds: getHours(endInterval),
-            milliseconds: getHours(endInterval),
-          });
-          dates[datesIndex].additionalStart = additionalStartInterval;
-          dates[datesIndex].additionalEnd = additionalEndInterval;
+    let dayOfWeekIndex = getDay(startInterval) === 6 ? 0 : getDay(startInterval) + 1;
+
+    let firstDay = true;
+    while (!isAfter(currDate, endDate)) {
+        dates[datesIndex] = {};
+        if (params.availableDays.includes(dayOfWeekIndex)) {
+            dates[datesIndex].isAvailable = true;
+        } else {
+            dates[datesIndex].isAvailable = false;
         }
-        tempStartInterval = setInterval(tempStartInterval, currDate);
-        tempEndInterval = set(new Date(), {
-          date: getDate(currDate),
-          hours: 23,
-          minutes: 59,
-          seconds: 59,
-          milliseconds: 59,
-        });
-        dates[datesIndex].startInterval = tempStartInterval;
-        dates[datesIndex].endInterval = tempEndInterval;
-      } else {
-        tempStartInterval = setInterval(tempStartInterval, currDate);
-        tempEndInterval = setInterval(tempEndInterval, currDate);
-        dates[datesIndex].startInterval = tempStartInterval;
-        dates[datesIndex].endInterval = tempEndInterval;
-      }
-    }
-    dates[datesIndex].startInterval = currDate;
-    currDate = addDays(currDate, 1);
-    datesIndex++;
-  }
+        dayOfWeekIndex++;
+        if (dayOfWeekIndex === 7) {
+            dayOfWeekIndex = 0;
+        }
 
-  while (isAfter(endOfWeek(endDate), currDate)) {
-    dates.push({ isAvailable: false, startInterval: currDate });
-    currDate = addDays(currDate, 1);
-  }
-  return dates;
+        if (dates[datesIndex].isAvailable) {
+            if (nextDayOverFlow) {
+                if (!firstDay) {
+                    let additionalStartInterval = startOfDay(currDate);
+                    let additionalEndInterval = set(new Date(), {
+                        date: getDate(currDate),
+                        hours: getHours(endInterval),
+                        minutes: getMinutes(endInterval),
+                        seconds: getHours(endInterval),
+                        milliseconds: getHours(endInterval),
+                    });
+                    dates[datesIndex].additionalStart = additionalStartInterval;
+                    dates[datesIndex].additionalEnd = additionalEndInterval;
+                }
+                tempStartInterval = setInterval(tempStartInterval, currDate);
+                tempEndInterval = set(new Date(), {
+                    date: getDate(currDate),
+                    hours: 23,
+                    minutes: 59,
+                    seconds: 59,
+                    milliseconds: 59,
+                });
+                dates[datesIndex].startInterval = tempStartInterval;
+                dates[datesIndex].endInterval = tempEndInterval;
+            } else {
+                tempStartInterval = setInterval(tempStartInterval, currDate);
+                tempEndInterval = setInterval(tempEndInterval, currDate);
+                dates[datesIndex].startInterval = tempStartInterval;
+                dates[datesIndex].endInterval = tempEndInterval;
+            }
+        }
+        dates[datesIndex].startInterval = currDate;
+        currDate = addDays(currDate, 1);
+        datesIndex++;
+    }
+
+    while (isAfter(endOfWeek(endDate), currDate)) {
+        dates.push({ isAvailable: false, startInterval: currDate });
+        currDate = addDays(currDate, 1);
+    }
+    return dates;
 }
 
 function setInterval(interval, currDate) {
-  return set(interval, {
-    date: getDate(currDate),
-  });
+    return set(interval, {
+        date: getDate(currDate),
+    });
 }
 
 export function getWeekInterval(startOfWeek) {
-  return [startOfWeek, set(addDays(startOfWeek, 6), { hours: 23, minutes: 59, seconds: 59, milliseconds: 99 })];
+    return [startOfWeek, set(addDays(startOfWeek, 6), { hours: 23, minutes: 59, seconds: 59, milliseconds: 99 })];
 }
