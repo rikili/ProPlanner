5855f9ed583de414ebf314c9f2fcfcaebb783614
Parham Alijani <palijani@students.cs.ubc.ca>
2023-06-10 16:43:32 -0700

    initialized starting dates from the store

--- pro-planner-client/src/components/TripHalfDay.jsx
+++ pro-planner-client/src/components/TripHalfDay.jsx
@@ -3,23 +3,26 @@ import { getDate, isSameDay, isSameMonth, isBefore, isAfter } from 'date-fns';
 import { useState } from 'react';
 
 export const TripHalfDay = props => {
-	
 	const handleSelection = () => {
-		// 
+		//
 		if (props.isSelectingDate) {
 			// TODO: take care of logic of double selecting the same date aka isSelectingDate == props.date
 			// this is the second click case
 			// we need to add the date range to the store from [isSelectingDate, props.date] ~> might need to be flipped
 			// props.date
-			// [ [s1, e1] , ... ] 
+			// [ [s1, e1] , ... ]
 			// clicking isSameDate(isSelectingDate, props.date)
 			if (isBefore(props.isSelectingDate, props.date)) {
-				props.setDateSelections( [...props.dateSelections, [props.isSelectingDate, props.date]] );
+				props.setDateSelections([
+					...props.dateSelections,
+					[props.isSelectingDate, props.date],
+				]);
 			} else {
-				props.setDateSelections( [...props.dateSelections, [props.date, props.isSelectingDate]] );
+				props.setDateSelections([
+					...props.dateSelections,
+					[props.date, props.isSelectingDate],
+				]);
 			}
-			
-
 		} else {
 			props.setIsSelectingDate(props.date);
 		}
@@ -40,15 +43,11 @@ export const TripHalfDay = props => {
 		}
 	};
 
-
-    // TODO: use the followoing attributes for hover effect
-	// onMouseEnter={handleMouseEnter} 
+	// TODO: use the followoing attributes for hover effect
+	// onMouseEnter={handleMouseEnter}
 	// onMouseLeave={handleMouseLeave}
 	return (
-		<div
-			className={'half-day' + ' ' + setActive() + ' ' + setHover()}
-			onClick={ handleSelection }
-		>	
+		<div className={'half-day' + ' ' + setActive() + ' ' + setHover()}>
 			{props.type == 'AM' && props.date && props.date.getDate()}{' '}
 		</div>
 	);
