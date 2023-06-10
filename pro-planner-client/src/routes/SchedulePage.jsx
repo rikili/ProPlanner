9b62496e1b515ec7807454fc5b4e5aeff2daf0db
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-06-10 01:07:13 -0700

    Revert "added routing for TripCalendar"
    This reverts commit cef332a6ca8a0cff21f5cdc0d78cf7342d716252.

--- pro-planner-client/src/routes/SchedulePage.jsx
+++ pro-planner-client/src/routes/SchedulePage.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 import WeeklyCalendar from '../components/WeeklyCalandar';
-import TripCalendar from '../components/TripCalendar'
 import { PLAN_TYPE } from '../redux/planParamSlice';
 
 const SchedulePage = () => {
@@ -20,7 +19,7 @@ const SchedulePage = () => {
     return <div>
         {planParams.name && (planParams.planType === PLAN_TYPE.OUTING)
         ? <WeeklyCalendar />
-        : <TripCalendar />}
+        : <div/>}
     </div>
 };
 
