a87d965e71318c9e2fa764a0aaa3f3d6acfb9a84
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-06-09 21:23:43 -0700

    reverting changes

--- pro-planner-client/src/index.js
+++ pro-planner-client/src/index.js
@@ -57,9 +57,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-	  <Provider store={store}>
-		  <RouterProvider router={router} />
-	  </Provider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
 	</React.StrictMode>
 );
 
