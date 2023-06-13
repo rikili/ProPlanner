c34ff1c45e8b514a5189dcdad09e7dbf6f8a81c4
R <rikli258@gmail.com>
2023-06-12 19:45:10 -0700

    reformat home page missing data error

--- pro-planner-client/src/routes/SchedulePage.jsx
+++ pro-planner-client/src/routes/SchedulePage.jsx
@@ -1,17 +1,22 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 
 import WeeklyCalendar from '../components/WeeklyCalandar';
-import { PLAN_TYPE } from '../constants';
+import { ERR_TYPE, PLAN_TYPE } from '../constants';
+import { setError } from "../redux/errorSlice";
 
 const SchedulePage = () => {
     const planParams = useSelector(state => state.planParameters);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!planParams.name) {
-            alert('Invalid plan details. Redirecting to start of plan creation.');
+            dispatch(setError({
+                errType: ERR_TYPE.ERR,
+                message: 'Information of this plan is invalid, malformed, or missing. Redirecting to the landing page.'
+            }));
             navigate('/');
         }
     }, [planParams, navigate]);
