4af5c26b179bc39cfc2b8c18915ab0261d5d9375
R <rikli258@gmail.com>
2023-06-12 20:39:23 -0700

    add redirect to toast close

--- pro-planner-client/src/components/ErrorToast.jsx
+++ pro-planner-client/src/components/ErrorToast.jsx
@@ -1,20 +1,26 @@
+import { forwardRef, useRef } from 'react';
 import { Toast } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { IoWarning } from 'react-icons/io5';
 import { MdDangerous } from 'react-icons/md';
 import { BsInfoCircleFill } from 'react-icons/bs';
+import { CgClose } from 'react-icons/cg';
 
 import { resetError } from '../redux/errorSlice';
 import { ERR_TYPE } from '../constants';
 import '../styles/ErrorToast.scss';
+import { useNavigate } from 'react-router-dom';
 
-const ErrorToast = () => {
+const ErrorToast = forwardRef((props, ref) => {
     const showError = useSelector(state => state.error.isShowError);
     const errType = useSelector(state => state.error.errorType);
     const errMsg = useSelector(state => state.error.errorMessage);
+    const errRedir = useSelector(state => state.error.redirect);
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const handleClose = () => {
+    const handleCloseToast = () => {
+        if (errRedir) navigate(errRedir);
         dispatch(resetError());
     }
 
@@ -34,18 +40,24 @@ const ErrorToast = () => {
     }
 
     return <>
-        {showError && <Toast className="m-3 position-relative err-toast" onClose={handleClose}>
-            <Toast.Header className="d-flex justify-content-between">
+        {showError 
+        && <Toast
+            className="m-3 position-relative err-toast"
+        >
+            <Toast.Header className="d-flex justify-content-between" closeButton={false}>
                 <div className="d-flex align-items-center">
                     {errIcon}
                     <strong className="ms-2">{errType}</strong>
                 </div>
+                <button className="toast-close" onClick={handleCloseToast} ref={ref}>
+                    <CgClose className="close-symbol" />
+                </button>
             </Toast.Header>
             <Toast.Body>
                 {errMsg}
             </Toast.Body>
         </Toast>}
     </>
-};
+});
 
 export default ErrorToast;
--- pro-planner-client/src/redux/errorSlice.js
+++ pro-planner-client/src/redux/errorSlice.js
@@ -6,30 +6,34 @@ const errorSlice = createSlice({
         isShowError: false,
         disableControl: false,
         errorType: null,
+        redirect: null,
         errorMessage: '',
     },
     reducers: {
         /* 
             Takes in payload of format
             {
-                errType = ERR_TYPE         // type of error
-                message = string       // message of error
-                disableControl? = bool   // dictates if error should block user action
+                errType = ERR_TYPE       // type of error
+                message = string        // message of error
+                redirect = string       // route to redirect on close
+                disableControl? = bool  // dictates if error should block user action
             }
         */
         setError(state, action) {
-            if (!action.type && !action.message && !action.redirect) {
+            const payload = action.payload;
+            if (!payload.type && !payload.message) {
                 return;
             }
             state.isShowError = true;
-            state.errorType = action.payload.errType;
-            state.errorMessage = action.payload.message;
-            state.disableControl = !!action.payload.disableControl;
+            state.errorType = payload.errType;
+            state.errorMessage = payload.message;
+            state.redirect = payload.redirect;
+            state.disableControl = !!payload.disableControl;
         },
         /* 
 
         */
-        resetError(state, action) {
+        resetError(state) {
             state.isShowError = false;
             state.disableControl = false;
             state.errorType = null;
