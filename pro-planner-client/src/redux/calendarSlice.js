aad124dece33956cb845f0b764006c177db8dab6
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-06-14 00:35:17 -0700

    refactored TripHalfDay, added more sample data

--- pro-planner-client/src/redux/calendarSlice.js
+++ pro-planner-client/src/redux/calendarSlice.js
@@ -4,12 +4,14 @@ const calendarSlice = createSlice({
 	name: 'calendar',
 	initialState: {
 		user1: [
-			[new Date('June 1, 2023 06:00:00'), new Date("June 30, 2023 06:00:00")],
+			[new Date('June 1, 2023 18:00:00'), new Date("June 30, 2023 18:00:00")],
 			[new Date('July 1, 2023 06:00:00'), new Date("July 31, 2023 06:00:00")]
-			// [new Date('July 3, 2023 18:00:00'), new Date("July 9, 2023 18:00:00")]
 		],
 		user2: [
 			[new Date('July 1, 2023 06:00:00'), new Date("July 31, 2023 06:00:00")]
+		],
+		user3: [
+			[new Date('June 25, 2023 06:00:00'), new Date("July 5, 2023 06:00:00")]
 		]
 	},
 	reducers: {},
