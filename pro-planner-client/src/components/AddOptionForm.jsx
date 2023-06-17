af3430a9891faf9fb7a1927996d963ef5ff0755b
Heeseop An <heeseop.an@gmail.com>
2023-06-17 00:25:51 -0700

    implement add new option component

--- pro-planner-client/src/components/AddOptionForm.jsx
+++ pro-planner-client/src/components/AddOptionForm.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Modal, Button, Form} from "react-bootstrap";
 
-function AddOptionForm() {
+function AddOptionForm({showModal, setShowModal}) {
+    const [newOption, setNewOption] = useState("")
+
+    const handleSaveOption = () => {
+        // TODO...
+        console.log(newOption);
+
+        handleCloseModal();
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setNewOption("");
+    }
+
+
     return (
-        <div></div>
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Option</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter new option"
+                        value={newOption}
+                        onChange={(e) => setNewOption(e.target.value)}
+                    />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={handleSaveOption}>
+                    Add Option
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
