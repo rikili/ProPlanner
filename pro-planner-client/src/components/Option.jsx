ebe9c532c7271a606fa81aac775397a060255c7c
Heeseop An <heeseop.an@gmail.com>
2023-06-17 02:06:04 -0700

    add poll progress bar

--- pro-planner-client/src/components/Option.jsx
+++ pro-planner-client/src/components/Option.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Form} from "react-bootstrap";
+import {Col, Container, Form, ProgressBar, Row} from "react-bootstrap";
 
 function Option({option}) {
     return (
         <>
-            <Form.Check
-                type={'radio'}
-                id={'default-radio'}
-                label={option.option}
-            />
+            <Container>
+                <Row>
+                    <Col>
+                        <Form.Check
+                            type={'radio'}
+                            id={'default-radio'}
+                            label={option.option}
+                        />
+                    </Col>
+                    <Col>
+                        <ProgressBar now={6} label={'6'}/>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
