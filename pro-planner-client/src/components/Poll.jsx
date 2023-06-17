a5f47b7e096f3913e5ee7e94130d2d20668d4cf4
Heeseop An <heeseop.an@gmail.com>
2023-06-17 00:25:51 -0700

    implement add new option component

--- pro-planner-client/src/components/Poll.jsx
+++ pro-planner-client/src/components/Poll.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import OptionsList from "./OptionsList";
 import {Button, Card} from "react-bootstrap";
+import {useState} from 'react';
+import AddOptionForm from "./AddOptionForm";
 
 function Poll({poll}) {
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleAddOption = () => {
+        setShowModal(true);
+    };
+
     return (
         <>
             <Card className='mt-4 p-4' style={{width: '700px'}}>
@@ -10,8 +19,8 @@ function Poll({poll}) {
                     <Card.Title>{poll.question}</Card.Title>
                     <OptionsList optionList={poll.optionList}/>
                     <div className="mb-2">
-                        <Button variant="primary" size='sm'>
-                            Add Option
+                        <Button variant="primary" size='sm' onClick={handleAddOption}>
+                            Add New Option
                         </Button>{' '}
                         <Button variant="primary" size='sm'>
                             Vote
@@ -19,6 +28,9 @@ function Poll({poll}) {
                     </div>
                 </Card.Body>
             </Card>
+
+            <AddOptionForm showModal={showModal} setShowModal={setShowModal}/>
+
         </>
     );
 }
