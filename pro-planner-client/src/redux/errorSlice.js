e650a1debf99520cd11cf58c2e19e9cb8c661b45
R <rikli258@gmail.com>
2023-06-19 12:13:20 -0700

    Undo reverting of "add more error toasts + cleanup"
    This reverts commit 710b852e121f07fa05c64b655fda9d91ae650103.

--- pro-planner-client/src/redux/errorSlice.js
+++ pro-planner-client/src/redux/errorSlice.js
@@ -38,6 +38,7 @@ const errorSlice = createSlice({
             state.disableControl = false;
             state.errorType = null;
             state.errorMessage = '';
+            state.redirect = null;
         }
     }
 });
