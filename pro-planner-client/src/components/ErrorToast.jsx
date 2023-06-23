1e3f22d651f9cdb5a6fd9ad07c45031c1379308f
Heeseop An <heeseop.an@gmail.com>
2023-06-23 01:47:21 -0700

    add toast styling

--- pro-planner-client/src/components/ErrorToast.jsx
+++ pro-planner-client/src/components/ErrorToast.jsx
@@ -1,14 +1,14 @@
-import { forwardRef } from 'react';
-import { Toast } from 'react-bootstrap';
-import { useSelector, useDispatch } from 'react-redux';
-import { IoWarning } from 'react-icons/io5';
-import { MdDangerous } from 'react-icons/md';
-import { BsInfoCircleFill } from 'react-icons/bs';
-import { CgClose } from 'react-icons/cg';
+import {forwardRef} from 'react';
+import {Toast} from 'react-bootstrap';
+import {useSelector, useDispatch} from 'react-redux';
+import {IoWarning} from 'react-icons/io5';
+import {MdDangerous} from 'react-icons/md';
+import {BsInfoCircleFill} from 'react-icons/bs';
+import {CgClose} from 'react-icons/cg';
 
-import { resetError } from '../redux/errorSlice';
-import { ERR_TYPE } from '../constants';
-import { useNavigate } from 'react-router-dom';
+import {resetError} from '../redux/errorSlice';
+import {ERR_TYPE} from '../constants';
+import {useNavigate} from 'react-router-dom';
 import './ErrorToast.scss';
 
 const ErrorToast = forwardRef((props, ref) => {
@@ -27,36 +27,38 @@ const ErrorToast = forwardRef((props, ref) => {
     let errIcon;
     switch (errType) {
         case ERR_TYPE.ERR:
-            errIcon = <MdDangerous className="error-icon" color="red" />
+            errIcon = <MdDangerous className="error-icon" color="red"/>
             break;
         case ERR_TYPE.WARN:
-            errIcon = <IoWarning className="error-icon" color="orange" />
+            errIcon = <IoWarning className="error-icon" color="orange"/>
             break;
         case ERR_TYPE.INFO:
-            errIcon = <BsInfoCircleFill className="error-icon" color="navy" />
+            errIcon = <BsInfoCircleFill className="error-icon" color="navy"/>
             break;
         default:
 
     }
 
     return <>
-        {showError 
-        && <Toast
-            className="m-3 position-relative err-toast"
-        >
-            <Toast.Header className="d-flex justify-content-between" closeButton={false}>
-                <div className="d-flex align-items-center">
-                    {errIcon}
-                    <strong className="ms-2">{errType}</strong>
-                </div>
-                <button className="toast-close" onClick={handleCloseToast} ref={ref}>
-                    <CgClose className="close-symbol" />
-                </button>
-            </Toast.Header>
-            <Toast.Body>
-                {errMsg}
-            </Toast.Body>
-        </Toast>}
+        {showError
+            && <Toast
+                className="m-3 position-relative err-toast"
+                // makes the toast appear above the grey shadow created by the Modal component
+                style={{zIndex: 9999}}
+            >
+                <Toast.Header className="d-flex justify-content-between" closeButton={false}>
+                    <div className="d-flex align-items-center">
+                        {errIcon}
+                        <strong className="ms-2">{errType}</strong>
+                    </div>
+                    <button className="toast-close" onClick={handleCloseToast} ref={ref}>
+                        <CgClose className="close-symbol"/>
+                    </button>
+                </Toast.Header>
+                <Toast.Body>
+                    {errMsg}
+                </Toast.Body>
+            </Toast>}
     </>
 });
 
