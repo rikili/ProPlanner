d42d58c7f224577291690706efe04b3fabe3b584
Heeseop An <heeseop.an@gmail.com>
2023-07-01 13:25:17 -0700

    add more error handling

--- pro-planner-client/src/index.js
+++ pro-planner-client/src/index.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '',
-                element: <VotePage />,
+                element: <LandingPage />,
                 errorElement: <ErrorPage />,
             },
             {
