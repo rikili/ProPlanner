0d68b428ca7270625fc19583a9d32cbcf45e080e
Vincent Wu <vincewu124@hotmail.com>
2023-07-08 23:51:02 -0700

    added timezone
    added timezone

--- pro-planner-server/helpers/timezone.js
+++ pro-planner-server/helpers/timezone.js
@@ -1,81 +1,69 @@
+// code references date-fns-tz: https://github.com/marnusw/date-fns-tz
 // exporting multiple functions references: https://stackoverflow.com/questions/33589571/module-exports-that-include-all-functions-in-a-single-line
-// converting timezone references: https://www.npmjs.com/package/date-fns-tz
+// converting timezone references: https://github.com/marnusw/date-fns-tz
 // getting substring by char references: https://stackoverflow.com/questions/9133102/how-to-grab-substring-before-a-specified-character-in-javascript
-// copying array references: https://stackoverflow.com/questions/7486085/copy-array-by-value
 
 const tz = require('date-fns-tz');
 const date_fns = require('date-fns');
 
-const firstHalf = '12am-12pm';
-const secondHalf = '12pm-11:00pm';
-
-const makeUTCDate = (month, day, year, hours, mins) => {
-  const resultDate = new Date();
-  resultDate.setUTCMonth(month);
-  resultDate.setUTCDate(day);
-  resultDate.setUTCFullYear(year);
-  resultDate.setUTCHours(hours);
-  resultDate.setUTCMinutes(mins);
-  resultDate.setUTCSeconds(0);
-  resultDate.setUTCMilliseconds(0);
-  return resultDate;
+const getOffset = (timezone) => {
+  return tz.getTimezoneOffset(timezone) / 3600000;
 };
 
-const templateDate = tz.format(new Date('7-1-2023 06:00:00'), 'yyyy-MM-dd HH:mm:ssXXX', { timeZone: 'America/Juneau' });
-const utcPrevCutoff = makeUTCDate(6, 1, 2023, 0, 0);
-const utcNextCutoff = makeUTCDate(6, 1, 2023, 12, 0);
+const makeAvailabilityDates = (date, timezone) => {
+  let months = Object.keys(date);
 
-const makeAvailabilityDates = (date) => {
-  // let months = Object.keys(date);
+  const offset = getOffset(timezone);
+  if (offset <= -6) {
+    // holds first day of first beginning month
+    const isFirstMonthDaySelected = date[months[0]][0][0] === true;
+    // todo: will refactor this with convertCalendarLocal()
+    for (let i = 0; i < months.length; i++) {
+      const monthDateSplit = months[i].split('-');
+      const daysOfMonth = date_fns.getDaysInMonth(new Date(`${parseInt(monthDateSplit[0]) + 1}-2-${monthDateSplit[1]}`));
+      for (let j = 0; j < daysOfMonth - 1; j++) {
+        date[months[i]][j][0] = date[months[i]][j][1];
+        date[months[i]][j][1] = date[months[i]][j + 1][0];
+      }
+      date[months[i]][daysOfMonth - 1][0] = date[months[i]][daysOfMonth - 1][1];
+      if (months[i + 1]) {
+        date[months[i]][daysOfMonth - 1][1] = date[months[i + 1]][0][0];
+      } else {
+        date[months[i]][daysOfMonth - 1][1] = false;
+      }
+    }
 
-  // const utcStartDay = tz.zonedTimeToUtc(templateDate, timezone);
-  // console.log(utcStartDay);
-  // const moveSlotBack = date_fns.isAfter(utcPrevCutoff, utcStartDay);
-  // const moveSlotForward = date_fns.isAfter(utcStartDay, utcNextCutoff);
+    // build new prev month
+    const prevMonthYear = createDateShift(months[0], 'prev');
+    const prevMonth = prevMonthYear[0];
+    const year = prevMonthYear[1];
+    const prevMonthDate = new Date(`${parseInt(prevMonth) + 1}-2-${year}`);
+    date[`${prevMonth}-${year}`] = createMonth(prevMonthDate);
+    if (isFirstMonthDaySelected) {
+      date[`${prevMonth}-${year}`][date_fns.getDaysInMonth(prevMonthDate) - 1] = [false, true];
+    }
+  } else if (offset >= 6) {
+    let replaceFirstHalfDayValue = date[months[0]][0][1] ? true : false;
+    // todo: will refactor this with convertCalendarLocal()
+    for (let i = 0; i < months.length; i++) {
+      const monthDateSplit = months[i].split('-');
+      daysOfMonth = date_fns.getDaysInMonth(new Date(`${parseInt(monthDateSplit[0]) + 1}-2-${monthDateSplit[1]}`));
+      for (let j = 0; j < daysOfMonth; j++) {
+        let replaceSecondHalfDayValue = date[months[i]][j][0] ? true : false;
+        date[months[i]][j][0] = replaceFirstHalfDayValue;
+        replaceFirstHalfDayValue = date[months[i]][j][1] ? true : false;
+        date[months[i]][j][1] = replaceSecondHalfDayValue;
+      }
+    }
+    date[months[0]][0][0] = false;
 
-  // if (moveSlotBack) {
-  //   // holds first day of first beginning month
-  //   const firstMonthDaySlot = date[months[0]][0].splice();
-
-  //   for (let i = 0; i < months.length - 1; i++) {
-  //     daysOfMonth = date_fns.getDaysInMonth(new Date(`${months[i][0]}-2-2023`));
-  //     for (let j = 0; j < daysOfMonth - 1; j++) {
-  //       date[months[i]][j][0] = date[months[i]][j][1];
-  //       date[months[i]][j][1] = date[months[i]][j + 1][0];
-  //     }
-  //     date[months[i]][daysOfMonth - 1][0] = date[months[i]][daysOfMonth - 1][1];
-  //     if (months[i + 1]) {
-  //       date[months[i]][daysOfMonth - 1][1] = date[months[i + 1]][0][0] ? date[months[i + 1]][0][0] : false;
-  //     } else {
-  //       date[months[i]][daysOfMonth - 1][1] = false;
-  //     }
-  //   }
-
-  //   // build new prev month
-  //   const prevMonth = months[0].split('-')[0] - 1;
-  //   const currYear = months[0].split('-')[1];
-  //   const prevMonthDate = new Date(`${prevMonth}-2-${currYear}`);
-  //   date[`${prevMonth}-${currYear}`] = createMonth(prevMonthDate);
-  //   if (firstMonthDaySlot[0] === true) {
-  //     date[`${prevMonth}-${currYear}`][date_fns.getDaysInMonth(prevMonthDate) - 1] = [false, true];
-  //   }
-  // } else if (moveSlotForward) {
-  //   // check very first one?
-  //   for (let i = 0; i < months.length - 1; i++) {
-  //     daysOfMonth = date_fns.getDaysInMonth(new Date(`${parseInt(months[0].split('-')[0]) + 1}-2-${months[0].split('-')[1]}`));
-  //     for (let j = 1; j < daysOfMonth; j++) {
-  //       date[months[i]][j][0] = date[months[i]][j - 1][1];
-  //       date[months[i]][j][1] = date[months[i]][j][0];
-  //     }
-  //     date[months[i]][daysOfMonth - 1][0] = date[months[i]][daysOfMonth - 1][1];
-  //   }
-
-  //   const nextMonth = months[months.length - 1].split('-')[0] + 1;
-  //   const currYear = months[months.length - 1].split('-')[1];
-  //   const nextMonthDate = new Date(`${nextMonth}-2-${currYear}`);
-  //   date[`${nextMonth}-${currYear}`] = createMonth(nextMonthDate);
-  //   date[`${nextMonth}-${currYear}`][0] = [true, false];
-  // }
+    const nextMonthYear = createDateShift(months[months.length - 1], 'next');
+    const nextMonth = nextMonthYear[0];
+    const year = nextMonthYear[1];
+    const nextMonthDate = new Date(`${parseInt(nextMonth) + 1}-2-${year}`);
+    date[`${nextMonth}-${year}`] = createMonth(nextMonthDate);
+    date[`${nextMonth}-${year}`][0] = [replaceFirstHalfDayValue, false];
+  }
   return date;
 };
 
@@ -88,7 +76,47 @@ const createMonth = (date) => {
   return month;
 };
 
+const createDateShift = (date, shiftType) => {
+  const splitDate = date.split('-');
+  const month = splitDate[0];
+  const year = splitDate[1];
+  if (shiftType === 'next') {
+    return [(parseInt(month) + 1) % 11, parseInt(month) + 1 > 11 ? parseInt(year) + 1 : year];
+  } else {
+    return [parseInt(month) === 0 ? '11' : month - 1, parseInt(month) === 0 ? parseInt(year) - 1 : year];
+  }
+};
+
+const convertCalendarLocal = (months, offset) => {
+  const finalMonth = [];
+  if (offset >= 6) {
+    // moved forward --> revert backward
+    for (let dayOfMonth = 0; dayOfMonth < months[0].length - 1; dayOfMonth++) {
+      finalMonth[dayOfMonth] = [];
+      finalMonth[dayOfMonth][0] = months[0][dayOfMonth][1];
+      finalMonth[dayOfMonth][1] = months[0][dayOfMonth + 1][0];
+    }
+    finalMonth[months[0].length - 1] = [];
+    finalMonth[months[0].length - 1][0] = months[0][months[0].length - 1][1];
+    finalMonth[months[0].length - 1][1] = months[1][0][0];
+  } else {
+    // moved backward --> revert forward
+    finalMonth[0] = [];
+    finalMonth[0][0] = months[0][months[0].length - 1][1];
+    finalMonth[0][1] = months[1][0][0];
+    for (let dayOfMonth = 1; dayOfMonth < months[1].length; dayOfMonth++) {
+      finalMonth[dayOfMonth] = [];
+      finalMonth[dayOfMonth][0] = months[1][dayOfMonth - 1][1];
+      finalMonth[dayOfMonth][1] = months[1][dayOfMonth][0];
+    }
+  }
+  return finalMonth;
+};
+
 module.exports = {
   makeAvailabilityDates,
   createMonth,
+  getOffset,
+  convertCalendarLocal,
+  createDateShift,
 };
--- pro-planner-server/routes/trip.js
+++ pro-planner-server/routes/trip.js
@@ -1,6 +1,6 @@
 // express routing references https://expressjs.com/en/guide/using-middleware.html
 // mongoose references https://mongoosejs.com/docs/index.html
-// updating object in array references: https://stackoverflow.com/questions/11372065/mongodb-how-do-i-update-a-single-subelement-in-an-array-referenced-by-the-inde
+// updating mongodb object in array references: https://stackoverflow.com/questions/11372065/mongodb-how-do-i-update-a-single-subelement-in-an-array-referenced-by-the-inde
 // finding element in embedded array references: https://www.mongodb.com/docs/manual/tutorial/query-array-of-documents/
 // updating object array references: https://sparkbyexamples.com/mongodb/update-objects-in-the-array-in-mongodb/?expand_article=1
 // checking if object is empty references: https://www.freecodecamp.org/news/check-if-an-object-is-empty-in-javascript/
@@ -9,6 +9,8 @@
 // projecting nested object references: https://stackoverflow.com/questions/70590047/projection-on-nested-object-mongodb-find-query
 // reading mongodb object references: https://stackoverflow.com/questions/44833736/object-keys-returns-unexpected-keys-on-mongodb-object-from-collection
 // finding key in object references: https://stackoverflow.com/questions/39018389/mongodb-find-key-on-nested-object-key-json
+// change array value by index references: https://stackoverflow.com/questions/5915789/how-to-replace-item-in-array
+// changing order or object keys references: https://stackoverflow.com/questions/6959817/changing-the-order-of-the-object-keys
 
 const express = require('express');
 const router = express.Router();
@@ -16,16 +18,11 @@ const trip = require('../models/trip');
 const timezone = require('../helpers/timezone');
 const { ObjectId } = require('mongodb');
 
-router.get('/', (req, res) => {
-  res.send('trip');
-});
-
-router.get('/:id', async (req, res) => {});
-
 router.post('/', async (req, res) => {
   // const userId = new ObjectId(req.body.userId);
+  // temp userId
   const userId = new ObjectId('649009f3cf33190dacb27a77');
-  const userSelections = timezone.makeAvailabilityDates(req.body.selections);
+  const userSelections = timezone.makeAvailabilityDates(req.body.selections, req.body.timezone);
   const tripModel = new trip({
     planParameters: {
       name: req.body.name,
@@ -42,49 +39,135 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const month = Object.keys(req.body.selection)[0];
-  const addedNewUserInfo = await addSelection(req.params.id, req.body.userId, month, req.body.selection[month]);
-  res.status(200).send({ month: addedNewUserInfo.toJSON().month });
+  const userTimezone = req.body.timezone;
+  const tripId = req.params.id;
+  const userId = req.body.userId;
+  let userSelection = timezone.makeAvailabilityDates(req.body.selection, userTimezone);
+  const selectionMonths = Object.keys(userSelection);
+
+  if (selectionMonths.length > 1) {
+    const offset = timezone.getOffset(userTimezone);
+
+    let currPrevNextMonth = await getMonth(tripId, userId, selectionMonths[1]);
+    currPrevNextMonth = currPrevNextMonth.toJSON();
+    if (currPrevNextMonth.month[0] !== null) {
+      if (offset >= 6) {
+        currPrevNextMonth.month[0][0][0] = userSelection[selectionMonths[1]][0][0];
+      } else {
+        currPrevNextMonth.month[0][currPrevNextMonth.month[0].length - 1][1] =
+          userSelection[selectionMonths[1]][userSelection[selectionMonths[1]].length - 1][1];
+      }
+      userSelection[selectionMonths[1]] = currPrevNextMonth.month[0];
+      // swapping order for convertCalendarLocal() to work
+      const swapOrder = {
+        [selectionMonths[1]]: null,
+        [selectionMonths[0]]: null,
+      };
+      userSelection = Object.assign(swapOrder, userSelection);
+    }
+  }
+
+  let addedNewUserInfo = await addSelection(tripId, userId, userSelection);
+  addedNewUserInfo = addedNewUserInfo.toJSON();
+  if (addedNewUserInfo.month[1].length > 3) {
+    addedNewUserInfo.month = timezone.convertCalendarLocal(addedNewUserInfo.month, timezone.getOffset(userTimezone));
+  }
+  res.status(200).send({ month: addedNewUserInfo.month });
 });
 
 router.get('/:id/:userId', async (req, res) => {
   const monthQuery = req.query.month;
   const userId = req.params.userId;
-  const month = `$userInfo.${userId}.${monthQuery}`;
-  let requestedMonth = await trip.find(
+  const userTimezone = req.query.timezone;
+  let monthProjection = [`$userInfo.${userId}.${monthQuery}`];
+
+  const offset = timezone.getOffset(userTimezone);
+  const nextMonth = timezone.createDateShift(monthQuery, 'next');
+  const prevMonth = timezone.createDateShift(monthQuery, 'prev');
+  if (offset >= 6) {
+    monthProjection.push(`$userInfo.${userId}.${parseInt(nextMonth[0])}-${nextMonth[1]}`);
+  } else if (offset <= -6) {
+    monthProjection.unshift(`$userInfo.${userId}.${parseInt(prevMonth[0])}-${prevMonth[1]}`);
+  }
+  let requestedMonth = await trip.findOne(
     { _id: new ObjectId(req.params.id), [`userInfo.${userId}`]: { $exists: true } },
     {
-      month: month,
+      month: monthProjection,
     }
   );
-  requestedMonth = requestedMonth[0].toJSON();
-  if (!requestedMonth.month) {
-    const newMonth = timezone.createMonth(new Date(`${monthQuery.split('-')[0]}-2-${monthQuery.split('-')[1]}`));
-    const addedNewMonth = await addSelection(req.params.id, req.params.userId, req.query.month, newMonth);
-    res.status(200).send({ month: addedNewMonth.toJSON().month });
+  requestedMonth = requestedMonth.toJSON();
+  if (requestedMonth.month.length > 1) {
+    // can only be -1, 0, or 1
+    const nullCheckIndex = requestedMonth.month.indexOf(null);
+    if (nullCheckIndex !== -1) {
+      const monthAfterOrBefore = nullCheckIndex
+        ? `${parseInt(nextMonth[0]) + 1}-2-${nextMonth[1]}`
+        : `${parseInt(prevMonth[0]) + 1}-2-${prevMonth[1]}`;
+      const tempMonth = timezone.createMonth(new Date(monthAfterOrBefore));
+      requestedMonth.month[nullCheckIndex] = tempMonth;
+    }
+    const updatedMonth = timezone.convertCalendarLocal(requestedMonth.month, offset);
+    res.status(200).send({ month: updatedMonth });
   } else {
-    res.status(200).send({ month: requestedMonth.month });
+    if (!requestedMonth.month) {
+      const newMonth = timezone.createMonth(new Date(`${parseInt(monthQuery.split('-')[0]) + 1}-2-${monthQuery.split('-')[1]}`));
+      res.status(200).send({ month: newMonth });
+    } else {
+      res.status(200).send({ month: requestedMonth.month[0] });
+    }
   }
 });
 
-const addSelection = async (tripId, userId, month, monthToAdd) => {
-  const monthPath = `$userInfo.${userId}.${month}`;
-  const addMonth = await trip.findOneAndUpdate(
-    { _id: new ObjectId(tripId) },
-    {
-      $set: {
-        [`userInfo.${userId}.${month}`]: monthToAdd,
+const addSelection = async (tripId, userId, monthToAdd) => {
+  const months = Object.keys(monthToAdd);
+  let addMonth;
+  if (months.length > 1) {
+    const monthPaths = [`$userInfo.${userId}.${months[0]}`, `$userInfo.${userId}.${months[1]}`];
+    addMonth = await trip.findOneAndUpdate(
+      { _id: new ObjectId(tripId) },
+      {
+        $set: {
+          [`userInfo.${userId}.${months[0]}`]: monthToAdd[months[0]],
+          [`userInfo.${userId}.${months[1]}`]: monthToAdd[months[1]],
+        },
       },
-    },
-    {
-      new: true,
-      projection: {
-        _id: 0,
-        month: monthPath,
+      {
+        new: true,
+        projection: {
+          _id: 0,
+          month: monthPaths,
+        },
+      }
+    );
+  } else {
+    const monthPath = `$userInfo.${userId}.${months[0]}`;
+    addMonth = await trip.findOneAndUpdate(
+      { _id: new ObjectId(tripId) },
+      {
+        $set: {
+          [`userInfo.${userId}.${months[0]}`]: monthToAdd[months[0]],
+        },
       },
+      {
+        new: true,
+        projection: {
+          _id: 0,
+          month: monthPath,
+        },
+      }
+    );
+  }
+  return addMonth;
+};
+
+const getMonth = async (id, userId, month) => {
+  let dbMonth = await trip.findOne(
+    { _id: new ObjectId(id), [`userInfo.${userId}`]: { $exists: true } },
+    {
+      month: [`$userInfo.${userId}.${month}`],
     }
   );
-  return addMonth;
+  return dbMonth;
 };
 
 module.exports = router;
