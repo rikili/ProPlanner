e120dccfcf01477d183ece18384926e60a8e1b9a
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-07-15 12:46:12 -0700

    add Map component into HomePage

--- pro-planner-client/src/routes/HomePage.jsx
+++ pro-planner-client/src/routes/HomePage.jsx
@@ -3,10 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLoaderData } from 'react-router-dom';
 import { updatePlan } from '../redux/planParamSlice';
 import { ERR_TYPE, LOAD_STATUS } from '../constants';
-import { setError } from "../redux/errorSlice";
-import NavigationBar from "../components/NavigationBar";
+import { setError } from '../redux/errorSlice';
+import NavigationBar from '../components/NavigationBar';
 import LoadingDisplay from '../components/LoadingDisplay';
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useNavigate } from 'react-router';
 import axios from 'axios';
 
 import './HomePage.scss';
@@ -26,35 +26,41 @@ const HomePage = () => {
 		if (!isUserSelected) {
 			navigate(`/user/${planId}`);
 		} else {
-			axios.get(buildServerRoute('plan', planId))
-				.then((result) => {
+			axios
+				.get(buildServerRoute('plan', planId))
+				.then(result => {
 					setParamStatus(LOAD_STATUS.SUCCESS);
 					dispatch(updatePlan(result.data));
 				})
-				.catch((err) => {
+				.catch(err => {
 					setParamStatus(LOAD_STATUS.FAILED);
 					if (err.response.status === 404) {
-						dispatch(setError({
-							errType: ERR_TYPE.ERR,
-							message: 'Information of this plan is invalid, malformed, or missing. Close this notification to be redirected to the landing page.',
-							redirect: '/',
-							disableControl: true
-						}));
+						dispatch(
+							setError({
+								errType: ERR_TYPE.ERR,
+								message:
+									'Information of this plan is invalid, malformed, or missing. Close this notification to be redirected to the landing page.',
+								redirect: '/',
+								disableControl: true,
+							})
+						);
 					}
 				});
 			setParamStatus(LOAD_STATUS.LOADING);
 		}
 	}, [dispatch, planId, isUserSelected, navigate]);
 
-	return <>
-		<div className='home-page'>
-			<NavigationBar planId={planId}/>
-			<div className='page-content'>
-				{ (paramStatus === LOAD_STATUS.LOADING) && <LoadingDisplay /> }
-				{ isParamsInit && <Outlet /> }
+	return (
+		<>
+			<div className="home-page">
+				<NavigationBar planId={planId} />
+				<div className="page-content">
+					{paramStatus === LOAD_STATUS.LOADING && <LoadingDisplay />}
+					{isParamsInit && <Outlet />}
+				</div>
 			</div>
-		</div>
-	</>;
+		</>
+	);
 };
 
 export default HomePage;
