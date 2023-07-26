54502851dd8d8c4bf1a94aa8f9ecc402f2af166e
Parham Alijani <palijani@students.cs.ubc.ca>
2023-07-26 16:47:54 -0700

    ApiModal Component work

--- pro-planner-client/src/components/InputDetailsForm.jsx
+++ pro-planner-client/src/components/InputDetailsForm.jsx
@@ -7,128 +7,144 @@ Google Maps Places Autocomplete API Docs: https://developers.google.com/maps/doc
 */
 
 import { useState, useRef, forwardRef, useImperativeHandle } from 'react';
+import { useSelector } from 'react-redux';
 import { Card, Form, Row, ButtonGroup, Button, Col } from 'react-bootstrap';
 import { useJsApiLoader, Autocomplete } from '@react-google-maps/api';
-
-
+import ApiModal from './ApiModal';
 
 const InputDetailsForm = forwardRef(({ title = false }, ref) => {
-
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: process.env.REACT_APP_API_KEY,
-
+	// const apiKey = useSelector(state => state.planParameters.apiKey);
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: process.env.REACT_APP_API_KEY,
 		libraries: ['places'],
 	});
 
-    const [selectedDays, setSelectedDays] = useState({
-        'Su': true,
-        'Mo': true,
-        'Tu': true,
-        'We': true,
-        'Th': true,
-        'Fr': true,
-        'Sa': true,
-    });
-    const formRef = useRef(null);
+	const [selectedDays, setSelectedDays] = useState({
+		Su: true,
+		Mo: true,
+		Tu: true,
+		We: true,
+		Th: true,
+		Fr: true,
+		Sa: true,
+	});
+	const formRef = useRef(null);
 
-    const formatSelectedDays = () => {
-        const result = [];
-        Object.entries(selectedDays).forEach(([_, isSelected], index) => {
-            if (isSelected) {
-                result.push(index);
-            }
-        });
-        return result;
-    }
+	const formatSelectedDays = () => {
+		const result = [];
+		Object.entries(selectedDays).forEach(([_, isSelected], index) => {
+			if (isSelected) {
+				result.push(index);
+			}
+		});
+		return result;
+	};
 
-    const reformatDateString = (inpString) =>  {
-        const splits = inpString.match(/[0-9]+/g);
-        return `${splits[1]}-${splits[2]}-${splits[0]}`;
-    }
+	const reformatDateString = inpString => {
+		const splits = inpString.match(/[0-9]+/g);
+		return `${splits[1]}-${splits[2]}-${splits[0]}`;
+	};
 
-    useImperativeHandle(ref, () => {
-        return {
-            retrieveData: () => {
-                return {
-                    name: formRef.current.formPlanName.value,
-                    location: formRef.current.formPlanLoc.value,
-                    budget: formRef.current.formPlanBudget.value,
-                    dateRange: [
-                        reformatDateString(formRef.current.formPlanStartDate.value),
-                        reformatDateString(formRef.current.formPlanEndDate.value)
-                    ],
-                    selectedDaysOfWeek: formatSelectedDays(),
-                };
-            }
-        }
-    });
+	useImperativeHandle(ref, () => {
+		return {
+			retrieveData: () => {
+				return {
+					name: formRef.current.formPlanName.value,
+					location: formRef.current.formPlanLoc.value,
+					budget: formRef.current.formPlanBudget.value,
+					dateRange: [
+						reformatDateString(formRef.current.formPlanStartDate.value),
+						reformatDateString(formRef.current.formPlanEndDate.value),
+					],
+					selectedDaysOfWeek: formatSelectedDays(),
+				};
+			},
+		};
+	});
 
-    const updateSelection = async (dayLabel) => {
-        const newSelection = {...selectedDays};
-        newSelection[dayLabel] = !selectedDays[dayLabel]
-        setSelectedDays(newSelection);
-    }
-    
-    
-    
+	const updateSelection = async dayLabel => {
+		const newSelection = { ...selectedDays };
+		newSelection[dayLabel] = !selectedDays[dayLabel];
+		setSelectedDays(newSelection);
+	};
 
+	const [modalShow, setModalShow] = useState(true);
 
-    if (!isLoaded) {
-        return <> Loading.. </>
-    }
+	if (!isLoaded) {
+		return <> Loading.. </>;
+	}
 
-    return <Card className="m-3">
-        {title && <Card.Title className="m-3 mb-0">
-            <h2>{title}</h2>
-        </Card.Title>}
-        <Card.Body className="ps-5 pe-5">
-            <h6><b>Details</b></h6>
-            <Form className="d-flex flex-column" ref={formRef} onSubmit={e => e.preventDefault()}>
-                <Form.Group controlId="formPlanName" className="mb-2">
-                    <Form.Label>Plan Name </Form.Label>
-                    <Form.Control type="text" placeholder="Name your plan" />
-                </Form.Group>
+	return (
+		<Card className="m-3">
+			{title && (
+				<Card.Title className="m-3 mb-0">
+					<h2>{title}</h2>
+				</Card.Title>
+			)}
+			<Card.Body className="ps-5 pe-5">
+				<ApiModal show={modalShow} onHide={() => setModalShow(false)} />
+				<h6>
+					<b>Details</b>
+				</h6>
+				<Form
+					className="d-flex flex-column"
+					ref={formRef}
+					onSubmit={e => e.preventDefault()}
+				>
+					<Form.Group controlId="formPlanName" className="mb-2">
+						<Form.Label>Plan Name </Form.Label>
+						<Form.Control type="text" placeholder="Name your plan" />
+					</Form.Group>
 
-                <Form.Group controlId="formPlanLoc" className="mb-2">
-                    <Form.Label>Location </Form.Label>
-                    <Autocomplete>
-                        <Form.Control type="text" placeholder="Name your destination"/>
-                    </Autocomplete>
-                </Form.Group>
+					<Form.Group controlId="formPlanLoc" className="mb-2">
+						<Form.Label>Location </Form.Label>
+						<Autocomplete>
+							<Form.Control type="text" placeholder="Name your destination" />
+						</Autocomplete>
+					</Form.Group>
 
-                <Form.Group controlId="formPlanBudget" className="mb-2">
-                    <Form.Label>Budget </Form.Label>
-                    <Form.Control type="number" placeholder="Choose your budget" />
-                </Form.Group>
+					<Form.Group controlId="formPlanBudget" className="mb-2">
+						<Form.Label>Budget </Form.Label>
+						<Form.Control type="number" placeholder="Choose your budget" />
+					</Form.Group>
 
-                <Row className="d-flex flex-row">
-                    <Form.Group as={Col} controlId="formPlanStartDate" className="mb-2">
-                        <Form.Label>Start Date </Form.Label>
-                        <Form.Control type="date" />
-                    </Form.Group>
+					<Row className="d-flex flex-row">
+						<Form.Group as={Col} controlId="formPlanStartDate" className="mb-2">
+							<Form.Label>Start Date </Form.Label>
+							<Form.Control type="date" />
+						</Form.Group>
 
-                    <Form.Group as={Col} controlId="formPlanEndDate" className="mb-4">
-                        <Form.Label>End Date </Form.Label>
-                        <Form.Control type="date" />
-                    </Form.Group>
-                </Row>
+						<Form.Group as={Col} controlId="formPlanEndDate" className="mb-4">
+							<Form.Label>End Date </Form.Label>
+							<Form.Control type="date" />
+						</Form.Group>
+					</Row>
 
-                <ButtonGroup size="sm" className="d-flex flex-wrap gap-1 mx-auto w-100" style={{maxWidth: "50%"}}>
-                    {Object.entries(selectedDays).map(([dayLabel, isSelected], index) => {
-                        return <Button
-                            className="rounded"
-                            variant={`${isSelected ? 'primary' : 'secondary'}`}
-                            active={isSelected}
-                            key={`${dayLabel}-${index}`}
-                            onClick={() => updateSelection(dayLabel)}
-                        >
-                            {dayLabel}
-                        </Button>
-                    })}
-                </ButtonGroup>
-            </Form>
-        </Card.Body>
-    </Card>
+					<ButtonGroup
+						size="sm"
+						className="d-flex flex-wrap gap-1 mx-auto w-100"
+						style={{ maxWidth: '50%' }}
+					>
+						{Object.entries(selectedDays).map(
+							([dayLabel, isSelected], index) => {
+								return (
+									<Button
+										className="rounded"
+										variant={`${isSelected ? 'primary' : 'secondary'}`}
+										active={isSelected}
+										key={`${dayLabel}-${index}`}
+										onClick={() => updateSelection(dayLabel)}
+									>
+										{dayLabel}
+									</Button>
+								);
+							}
+						)}
+					</ButtonGroup>
+				</Form>
+			</Card.Body>
+		</Card>
+	);
 });
 
 export default InputDetailsForm;
--- pro-planner-client/src/redux/planParamSlice.js
+++ pro-planner-client/src/redux/planParamSlice.js
@@ -2,43 +2,50 @@ import { createSlice } from '@reduxjs/toolkit';
 import { PLAN_TYPE } from '../constants';
 
 const planParamSlice = createSlice({
-    name: 'parameters',
-    initialState: {
-        name: null,
-        planType: PLAN_TYPE.OUTING,
-        dateTimeRange: [],
-        dayOffset: [],
-        isAllDay: false,
-        location: null,
-        budget: null
-    },
-    reducers: {
-        // payload should be a string of either 'Trip' or 'Outing'
-        changePlanType(state, action) {
-            switch(action.payload) {
-                case PLAN_TYPE.TRIP:
-                    state.planType = action.payload;
-                    break;
-                case PLAN_TYPE.OUTING:
-                    state.planType = action.payload;
-                    break;
-                default:
-                    console.error('Invalid plan type provided to state.'); // TODO: implement an error/alert system to users
-            }
-        },
+	name: 'parameters',
+	initialState: {
+		name: null,
+		planType: PLAN_TYPE.OUTING,
+		dateTimeRange: [],
+		dayOffset: [],
+		isAllDay: false,
+		location: null,
+		budget: null,
+		apiKey: '',
+	},
+	reducers: {
+		// payload should be a string of either 'Trip' or 'Outing'
+		changePlanType(state, action) {
+			switch (action.payload) {
+				case PLAN_TYPE.TRIP:
+					state.planType = action.payload;
+					break;
+				case PLAN_TYPE.OUTING:
+					state.planType = action.payload;
+					break;
+				default:
+					console.error('Invalid plan type provided to state.'); // TODO: implement an error/alert system to users
+			}
+		},
 
-        // payload contains complete set of values to update -- should be complete even if values don't change
-        updatePlan(state, action) {
-            const input = action.payload;
-            state.name = input.name;
-            state.dayOffset = input.dayOffset;
-            state.isAllDay = input.isAllDay;
-            state.location = input.location;
-            state.budget = input.budget;
-            state.dateTimeRange = input.dateTimeRange;
-        }
-    },
+		// payload contains complete set of values to update -- should be complete even if values don't change
+		updatePlan(state, action) {
+			const input = action.payload;
+			state.name = input.name;
+			state.dayOffset = input.dayOffset;
+			state.isAllDay = input.isAllDay;
+			state.location = input.location;
+			state.budget = input.budget;
+			state.dateTimeRange = input.dateTimeRange;
+		},
+
+		updateApiKey(state, action) {
+			const apiKey = action.payload;
+			state.apiKey = apiKey;
+		},
+	},
 });
 
-export const { changePlanType, updatePlan } = planParamSlice.actions;
+export const { changePlanType, updatePlan, updateApiKey } =
+	planParamSlice.actions;
 export default planParamSlice.reducer;
