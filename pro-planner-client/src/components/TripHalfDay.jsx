3dbdf15a3af07b102bb6cd61d1cd038b94d75d5a
Parham Alijani <palijani@students.cs.ubc.ca>
2023-07-28 00:02:32 -0700

    console log cleanup

--- pro-planner-client/src/components/TripHalfDay.jsx
+++ pro-planner-client/src/components/TripHalfDay.jsx
@@ -5,85 +5,94 @@ import { assembleClass } from '../helpers/Utils';
 import './TripCalendar.scss';
 import { setDetailedUsers, setDetailedDay } from '../redux/tripSummarySlice';
 
-const stepArr = [[0, 0.25], [0.26, 0.5], [0.51, 0.75], [0.76, 1]];
+const stepArr = [
+	[0, 0.25],
+	[0.26, 0.5],
+	[0.51, 0.75],
+	[0.76, 1],
+];
 
 const TripHalfDay = ({
-    topHalf,
-    date,
-    editing,
-    deciding,
-    isDecisionSelect,
-    onMouseEnter,
-    onClick,
-    selections,
-    maxUsers,
-    isDecided,
-    isSelected,
-    isValid,
-    isPreviewed,
-    className,
+	topHalf,
+	date,
+	editing,
+	deciding,
+	isDecisionSelect,
+	onMouseEnter,
+	onClick,
+	selections,
+	maxUsers,
+	isDecided,
+	isSelected,
+	isValid,
+	isPreviewed,
+	className,
 }) => {
-    const dispatch = useDispatch();
-    const detailedDay = useSelector(state => state.tripSummary.detailedDay);
-    let isDetailedDay = false;
-    if (detailedDay) {
-        isDetailedDay = isEqual(new Date(detailedDay), date);
-    }
+	const dispatch = useDispatch();
+	const detailedDay = useSelector(state => state.tripSummary.detailedDay);
+	let isDetailedDay = false;
+	if (detailedDay) {
+		isDetailedDay = isEqual(new Date(detailedDay), date);
+	}
 
-    const handleClick = () => {
-        if (editing || deciding) {
-            onClick();
-            return;
-        }
-        if (isDetailedDay) {
-            console.log('clear select');
-            dispatch(setDetailedDay(null));
-            dispatch(setDetailedUsers([]));
-        } else {
-            dispatch(setDetailedDay(date.toISOString()));
-            dispatch(setDetailedUsers(selections));
-        }
-    }
+	const handleClick = () => {
+		if (editing || deciding) {
+			onClick();
+			return;
+		}
+		if (isDetailedDay) {
+			dispatch(setDetailedDay(null));
+			dispatch(setDetailedUsers([]));
+		} else {
+			dispatch(setDetailedDay(date.toISOString()));
+			dispatch(setDetailedUsers(selections));
+		}
+	};
 
-    let step;
-    if (!editing) {
-        const ratio = selections.length / maxUsers;
-        step = stepArr.reduce((acc, [lower, upper], index) => {
-            if (acc === null) {
-                if (ratio >= lower && ratio <= upper) {
-                    return index + 1;
-                }
-            }
-            return acc;
-        }, null);
-    }
+	let step;
+	if (!editing) {
+		const ratio = selections.length / maxUsers;
+		step = stepArr.reduce((acc, [lower, upper], index) => {
+			if (acc === null) {
+				if (ratio >= lower && ratio <= upper) {
+					return index + 1;
+				}
+			}
+			return acc;
+		}, null);
+	}
 
-    let classState;
-    if (editing) {
-        classState = assembleClass(isPreviewed && 'trip-preview', isSelected && 'trip-edit');
-    } else {
-        classState = assembleClass(
-            selections.length && `trip-selected trip-selected-${step}`,
-            (isDecided || isDecisionSelect) && `trip-decided-${step}`,
-        );
-    }
+	let classState;
+	if (editing) {
+		classState = assembleClass(
+			isPreviewed && 'trip-preview',
+			isSelected && 'trip-edit'
+		);
+	} else {
+		classState = assembleClass(
+			selections.length && `trip-selected trip-selected-${step}`,
+			(isDecided || isDecisionSelect) && `trip-decided-${step}`
+		);
+	}
 
-    const compiledClass = assembleClass(
-        className,
-        isValid && classState + ' valid',
-        !isValid && 'trip-invalid',
-        (isDetailedDay && !deciding) ? 'trip-detailed' : (deciding && isPreviewed && 'trip-decided-preview'),
-        );
+	const compiledClass = assembleClass(
+		className,
+		isValid && classState + ' valid',
+		!isValid && 'trip-invalid',
+		isDetailedDay && !deciding
+			? 'trip-detailed'
+			: deciding && isPreviewed && 'trip-decided-preview'
+	);
 
-    return (
-        <div
-            onClick={handleClick}
-            onMouseEnter={(editing || deciding) ? onMouseEnter : null}
-            className={compiledClass}
-        >
-            {topHalf && <div className='trip-half-label'>{format(date, 'd')}</div>}
-        </div>
-    );
+	return (
+		<div
+			onClick={handleClick}
+			onMouseEnter={editing || deciding ? onMouseEnter : null}
+			className={compiledClass}
+		>
+			{topHalf && <div className="trip-half-label">{format(date, 'd')}</div>}
+		</div>
+	);
 };
 
 export default TripHalfDay;
