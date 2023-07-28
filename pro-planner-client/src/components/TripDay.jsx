c0fe86949823db18f526f6b98d20edfe6eb923f7
Parham Alijani <palijani@students.cs.ubc.ca>
2023-07-27 22:51:48 -0700

    user side bar implemented

--- pro-planner-client/src/components/TripDay.jsx
+++ pro-planner-client/src/components/TripDay.jsx
@@ -2,76 +2,93 @@ import { Container } from 'react-bootstrap';
 import { getHalfDate } from '../helpers/TripCalendar';
 import TripHalfDay from './TripHalfDay';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const TripDay = ({
-    fake,
-    date,
-    selections,
-    editing,
-    deciding,
-    decisionSelect,
-    onMouseEnter,
-    onClick,
-    maxUsers,
-    isSelected,
-    checkValid,
-    checkPreview,
-    className,
+	fake,
+	date,
+	selections,
+	editing,
+	deciding,
+	decisionSelect,
+	onMouseEnter,
+	onClick,
+	maxUsers,
+	isSelected,
+	checkValid,
+	checkPreview,
+	className,
+	selectedUser,
 }) => {
-    const decisionRange = useSelector(state => state.planParameters.decisionRange);
-    if (fake) return <div className={`${className} fake-day`} />
+	const decisionRange = useSelector(
+		state => state.planParameters.decisionRange
+	);
 
-    const firstHalfDate = getHalfDate(date, true);
-    const secHalfDate = getHalfDate(date, false);
-    
-    const inDecisionRange = (date) => {
-        if (!decisionRange.length) return false;
-        if (deciding) return false;
-        return new Date(decisionRange[0]) <= date && date <= new Date(decisionRange[1]);
-    }
+	if (fake) return <div className={`${className} fake-day`} />;
 
-    const inDecisionSelect = (date) => {
-        if (!decisionSelect) return false;
-        return decisionSelect[0] <= date && date <= decisionSelect[1];
-    }
+	const firstHalfDate = getHalfDate(date, true);
+	const secHalfDate = getHalfDate(date, false);
 
-    return (
-        <Container className="trip-half-container" key={`day-container`}>
-            <TripHalfDay
-                topHalf={true}
-                date={firstHalfDate}
-                editing={editing}
-                deciding={deciding}
-                isDecisionSelect={inDecisionSelect(firstHalfDate)}
-                onMouseEnter={() => onMouseEnter(firstHalfDate)}
-                onClick={() => onClick(firstHalfDate)}
-                selections={editing ? null : selections[0]}
-                maxUsers={maxUsers}
-                isDecided={inDecisionRange(firstHalfDate)}
-                isSelected={editing ? isSelected.first : null}
-                isValid={checkValid(firstHalfDate)}
-                isPreviewed={checkPreview(firstHalfDate)}
-                className={className}
-                key={`day-first`}
-            />
-            <TripHalfDay
-                date={secHalfDate}
-                editing={editing}
-                deciding={deciding}
-                isDecisionSelect={inDecisionSelect(secHalfDate)}
-                onMouseEnter={() => onMouseEnter(secHalfDate)}
-                onClick={() => onClick(secHalfDate)}
-                selections={editing ? null : selections[1]}
-                maxUsers={maxUsers}
-                isDecided={inDecisionRange(secHalfDate)}
-                isSelected={editing ? isSelected.second : null}
-                isValid={checkValid(secHalfDate)}
-                isPreviewed={checkPreview(secHalfDate)}
-                className={className}
-                key={`day-second`}
-            />
-        </Container>
-    );
+	const inDecisionRange = date => {
+		if (!decisionRange.length) return false;
+		if (deciding) return false;
+		return (
+			new Date(decisionRange[0]) <= date && date <= new Date(decisionRange[1])
+		);
+	};
+
+	const inDecisionSelect = date => {
+		if (!decisionSelect) return false;
+		return decisionSelect[0] <= date && date <= decisionSelect[1];
+	};
+
+	// responsible for filtering based on user selected in the sidebar.
+	if (selectedUser) {
+		let result = [];
+		for (let selection of selections) {
+			selection = selection.filter(user => user === selectedUser);
+			result.push(selection);
+		}
+		selections = result;
+	}
+
+	return (
+		<Container className="trip-half-container" key={`day-container`}>
+			<TripHalfDay
+				topHalf={true}
+				date={firstHalfDate}
+				editing={editing}
+				deciding={deciding}
+				isDecisionSelect={inDecisionSelect(firstHalfDate)}
+				onMouseEnter={() => onMouseEnter(firstHalfDate)}
+				onClick={() => onClick(firstHalfDate)}
+				selections={editing ? null : selections[0]}
+				maxUsers={maxUsers}
+				isDecided={inDecisionRange(firstHalfDate)}
+				isSelected={editing ? isSelected.first : null}
+				isValid={checkValid(firstHalfDate)}
+				isPreviewed={checkPreview(firstHalfDate)}
+				className={className}
+				key={`day-first`}
+			/>
+			<TripHalfDay
+				date={secHalfDate}
+				editing={editing}
+				deciding={deciding}
+				isDecisionSelect={inDecisionSelect(secHalfDate)}
+				onMouseEnter={() => onMouseEnter(secHalfDate)}
+				onClick={() => onClick(secHalfDate)}
+				selections={editing ? null : selections[1]}
+				maxUsers={maxUsers}
+				isDecided={inDecisionRange(secHalfDate)}
+				isSelected={editing ? isSelected.second : null}
+				isValid={checkValid(secHalfDate)}
+				isPreviewed={checkPreview(secHalfDate)}
+				className={className}
+				key={`day-second`}
+			/>
+		</Container>
+	);
 };
 
 export default TripDay;
---
+++ pro-planner-client/src/components/UserSideBar.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Card, Col, ListGroup, Row } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+const UserSideBar = ({ isEditMode, selectedUser, setSelectedUser }) => {
+	const userList = useSelector(state => state.user.userList);
+	const loggedInUser = useSelector(state => state.user.selectedUser);
+
+	return (
+		<div className="d-flex flex-column justify-content-center align-items-center">
+			<Card className="details-card summary-list">
+				<Card.Header className="summary-body text-center">
+					<b>Responders</b>
+				</Card.Header>
+				<Card.Body className="summary-body">
+					<ListGroup
+						variant="flush"
+						style={{ maxHeight: '300px', overflow: 'scroll' }}
+					>
+						{isEditMode ? (
+							<>
+								<ListGroup.Item className="text-center" disabled as="h6">
+									All Users
+								</ListGroup.Item>
+								<ListGroup.Item className="text-center" active>
+									{loggedInUser}
+								</ListGroup.Item>
+								{userList.map(
+									user =>
+										loggedInUser !== user && (
+											<ListGroup.Item
+												className="text-center"
+												active={user === loggedInUser}
+												disabled={user !== loggedInUser}
+											>
+												{user}
+											</ListGroup.Item>
+										)
+								)}
+							</>
+						) : (
+							<>
+								<ListGroup.Item
+									className="text-center"
+									onClick={() => setSelectedUser(null)}
+									active={!selectedUser}
+									as="h6"
+								>
+									All Users
+								</ListGroup.Item>
+								<ListGroup.Item
+									className="text-center"
+									onClick={() => setSelectedUser(loggedInUser)}
+									active={selectedUser === loggedInUser}
+								>
+									{loggedInUser}
+								</ListGroup.Item>
+								{userList.map(
+									user =>
+										loggedInUser !== user && (
+											<ListGroup.Item
+												className="text-center"
+												onClick={() => setSelectedUser(user)}
+												active={user === selectedUser}
+											>
+												{user}
+											</ListGroup.Item>
+										)
+								)}
+							</>
+						)}
+					</ListGroup>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
+
+export default UserSideBar;
