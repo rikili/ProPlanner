d30be27ecd4a685cc6ecc72c564a61e87b6cd7ec
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-07-28 18:09:00 -0700

    refactored Cost Splitting components and redux structure

--- pro-planner-client/src/redux/costSlice.js
+++ pro-planner-client/src/redux/costSlice.js
@@ -1,47 +1,58 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { LOAD_STATUS } from "../constants";
+import axios from "axios";
+import { buildServerRoute } from "../helpers/Utils";
 
+export const getCostAsync = createAsyncThunk('cost/get', async({tripId}) => {
+    const response = await axios.get(buildServerRoute('cost', tripId));
+    return response.data;
+});
 const costSlice = createSlice({
     name: 'cost',
     initialState: {
-        user1: {
-            userName: 'User 1',
-            expenses: {
-                expense1: {
-                    item: "Go Kart tickets",
-                    amount: 120
+        costsID: null, 
+        costsStatus: null,
+        costs: {
+            user1: {
+                userName: 'User 1',
+                expenses: {
+                    expense1: {
+                        item: "Go Kart tickets",
+                        amount: 120
+                    },
+                    expense2: {
+                        item: "Movies",
+                        amount: 80
+                    }
                 },
-                expense2: {
-                    item: "Movies",
-                    amount: 80
-                }
             },
-        },
-        user2: {
-            userName: 'User 2',
-            expenses: {},
-        },
-        user3: {
-            userName: 'User 3',
-            expenses: {
-                expense1: {
-                    item: "PNE tickets",
-                    amount: 200
+            user2: {
+                userName: 'User 2',
+                expenses: {},
+            },
+            user3: {
+                userName: 'User 3',
+                expenses: {
+                    expense1: {
+                        item: "PNE tickets",
+                        amount: 200
+                    },
+                    expense2: {
+                        item: "Coffee",
+                        amount: 10
+                    }
                 },
-                expense2: {
-                    item: "Coffee",
-                    amount: 10
-                }
             },
+            user4: {
+                userName: 'User 4',
+                expenses: {
+                    expense1: {
+                        item: "Parking",
+                        amount: 30
+                    }
+                },
+            }  
         },
-        user4: {
-            userName: 'User 4',
-            expenses: {
-                expense1: {
-                    item: "Parking",
-                    amount: 30
-                }
-            },
-        }
     },
     reducers: {
         addExpense: (state, action) => {
@@ -50,11 +61,11 @@ const costSlice = createSlice({
                 item: input.newItem,
                 amount: input.newItemAmount
             }
-            state[input.userId].expenses[input.expenseId] = newExpense
+            state.costs[input.userId].expenses[input.expenseId] = newExpense
         },
         removeExpense: (state, action) => {
             const input = action.payload;
-            delete state[input.userId].expenses[input.expenseId];
+            delete state.costs[input.userId].expenses[input.expenseId];
         },
         addUser: (state, action) => {
             const input = action.payload
@@ -62,8 +73,24 @@ const costSlice = createSlice({
                 userName: input.name,
                 expenses: {}
             }
-            state[input.id] = newUser
+            state.costs[input.id] = newUser
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getCostAsync.pending, (state, action) => {
+            state.costsStatus = LOAD_STATUS.LOADING;
+        });
+        builder.addCase(getCostAsync.fulfilled, (state, action) => {
+            // state.costs = action.payload.costs;
+            console.log(action.payload)
+            state.pollStatus = LOAD_STATUS.SUCCESS;
+        });
+        builder.addCase(getCostAsync.rejected, (state, action) => {
+            state.pollStatus = LOAD_STATUS.FAILED;
+        });
+        // builder.addCase(addExpensesAsync.pending, (state, action) => {
+
+        // });
     }
 })
 
--- pro-planner-client/src/routes/CostPage.jsx
+++ pro-planner-client/src/routes/CostPage.jsx
@@ -1,20 +1,37 @@
 import TripExpense from "../components/TripExpense";
 import ExpenseSplit from "../components/ExpenseSplit";
+import LoadingDisplay from "../components/LoadingDisplay"
+import { getCostAsync } from "../redux/costSlice";
 import { Row, Col, Container } from "react-bootstrap"
+import { useLocation } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import React, {useEffect} from "react";
+import {LOAD_STATUS} from "../constants";
 
 const CostPage = () => {
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <TripExpense />
-                </Col>
-                <Col>
-                    <ExpenseSplit />
-                </Col>
-            </Row>
 
-        </Container>
+    const tripId = useLocation().pathname.split('/')[1];
+    const loadingState = useSelector((state) => state.cost.costStatus);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCostAsync({tripId}))
+    }, [dispatch, tripId])
+
+    return (
+        <>
+            {(loadingState === LOAD_STATUS.LOADING) && <LoadingDisplay/>}
+            <Container>
+                <Row>
+                    <Col>
+                        <TripExpense />
+                    </Col>
+                    <Col>
+                        <ExpenseSplit />
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
