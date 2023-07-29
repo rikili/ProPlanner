9d8378e0ae80e2bd1d8e023c85dfa002dc2ff0b2
Parham Alijani <palijani@students.cs.ubc.ca>
2023-07-28 18:23:04 -0700

    Map integrated

--- pro-planner-client/src/routes/OverviewPage.jsx
+++ pro-planner-client/src/routes/OverviewPage.jsx
@@ -1,59 +1,57 @@
 import React from 'react';
-import NavigationBar from "../components/NavigationBar";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import NavigationBar from '../components/NavigationBar';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './OverviewPage.scss';
-import ExpenseSplit from "../components/ExpenseSplit";
-import {useSelector} from "react-redux";
+import ExpenseSplit from '../components/ExpenseSplit';
+import { useSelector } from 'react-redux';
+import Map from '../components/Map';
 
 const OverviewPage = () => {
+	// const tripName = useSelector((state) => state.planParameters.name);
+	// const location = useSelector((state) => state.planParameters.location);
+	const decidedDates = useSelector(state => state.planParameters.decisionRange); // TODO: process decidedDates by plan type
+	const tripName = 'Trip Name'; // testing purpose
+	const location = 'Vancouver'; // testing purpose
 
-    // const tripName = useSelector((state) => state.planParameters.name);
-    // const location = useSelector((state) => state.planParameters.location);
-    const decidedDates = useSelector((state) => state.planParameters.decisionRange); // TODO: process decidedDates by plan type
-    const tripName = 'Trip Name'; // testing purpose
-    const location = 'Vancouver'; // testing purpose
-
-    return (
-        <>
-            <NavigationBar/>
-            <Container className="overview-container">
-                <Row className="overview-header-row">
-                    {tripName}
-                </Row>
-                <Row className="overview-map-row">
-                    <Card className="overview-map">
-                        Map
-                    </Card>
-                </Row>
-                <Row className="overview-details-cost-row">
-                    <Col className="overview-details-col">
-                        <Card className="overview-details-card">
-                            <Card.Body>
-                                <Card.Title>Details</Card.Title>
-                                <Card.Subtitle>Dates:</Card.Subtitle>
-                                <Card.Text>
-                                    {/*TODO: process decidedDates by plan type */}
-                                    {decidedDates.length > 0 ? decidedDates : `Dates not set`}
-                                </Card.Text>
-                                <Card.Subtitle>Location:</Card.Subtitle>
-                                <Card.Text>{location}</Card.Text>
-                                <Card.Subtitle>Description:</Card.Subtitle>
-                                <Card.Text>Bring umbrella</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className="overview-cost-col">
-                        <Card className="overview-cost-card">
-                            <Card.Body>
-                                <Card.Title>Costs</Card.Title>
-                                <ExpenseSplit/>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-}
+	return (
+		<>
+			<NavigationBar />
+			<Container className="overview-container">
+				<Row className="overview-header-row">{tripName}</Row>
+				<Row className="overview-map-row">
+					<Card className="overview-map">
+						<Map />
+					</Card>
+				</Row>
+				<Row className="overview-details-cost-row">
+					<Col className="overview-details-col">
+						<Card className="overview-details-card">
+							<Card.Body>
+								<Card.Title>Details</Card.Title>
+								<Card.Subtitle>Dates:</Card.Subtitle>
+								<Card.Text>
+									{/*TODO: process decidedDates by plan type */}
+									{decidedDates.length > 0 ? decidedDates : `Dates not set`}
+								</Card.Text>
+								<Card.Subtitle>Location:</Card.Subtitle>
+								<Card.Text>{location}</Card.Text>
+								<Card.Subtitle>Description:</Card.Subtitle>
+								<Card.Text>Bring umbrella</Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col className="overview-cost-col">
+						<Card className="overview-cost-card">
+							<Card.Body>
+								<Card.Title>Costs</Card.Title>
+								<ExpenseSplit />
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
+};
 
 export default OverviewPage;
