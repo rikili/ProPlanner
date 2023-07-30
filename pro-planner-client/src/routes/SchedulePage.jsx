0fce0ea06c624188fa68eeaac6d27542f20a7e70
R <rikli258@gmail.com>
2023-07-30 13:00:46 -0700

    fix trip user filter

--- pro-planner-client/src/routes/SchedulePage.jsx
+++ pro-planner-client/src/routes/SchedulePage.jsx
@@ -10,12 +10,12 @@ const SchedulePage = () => {
     const tripId = useLocation().pathname.slice(1);
     const planParams = useSelector(state => state.planParameters);
 
-    return <CalendarGroup>
-        {(planParams.planType === PLAN_TYPE.OUTING)
-            ? <OutingCalendar tripId={tripId} />
-            : <TripCalendar tripId={tripId} />
-        }
-    </CalendarGroup>
+    const renderCalendar = (selectedUser) => (planParams.planType === PLAN_TYPE.OUTING)
+        ? (<OutingCalendar tripId={tripId} selectedUser={selectedUser} />)
+        : (<TripCalendar tripId={tripId} selectedUser={selectedUser} />);
+
+
+    return <CalendarGroup renderCalendar={renderCalendar}/>
 };
 
 export default SchedulePage;
