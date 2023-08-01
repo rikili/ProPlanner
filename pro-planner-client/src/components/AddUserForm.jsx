282711e09befc0c404c65a3bdf9dee607ef85513
Parham Alijani <palijani@students.cs.ubc.ca>
2023-08-01 01:43:22 -0700

    backend connected

--- pro-planner-client/src/components/AddUserForm.jsx
+++ pro-planner-client/src/components/AddUserForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {useState} from "react";
-import {Button, Form,} from "react-bootstrap";
-import {addUser} from "../redux/userSlice";
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { addUserAsync } from '../redux/userSlice';
+import { useLocation } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { setError, resetError } from '../redux/errorSlice';
@@ -11,75 +12,89 @@ const MAX_NAME_LIMIT = 45;
 const NAME_CHECK_REGEX = /[^0-9a-zA-Z\s]+/g;
 
 const AddUserForm = () => {
-    const [userName, setUserName] = useState('');
-    const users = useSelector(state => state.user.userList);
-    const dispatch = useDispatch();
+	const planId = useLocation().pathname.split('/')[2];
+	const [userName, setUserName] = useState('');
+	const users = useSelector(state => state.user.userList);
+	const dispatch = useDispatch();
 
-    const handleUserNameChange = (e) => {
-        const inputVal = e.target.value;
-        if (inputVal.length >= MAX_NAME_LIMIT) {
-            setUserName(inputVal.slice(0, MAX_NAME_LIMIT));
-        } else {
-            setUserName(e.target.value);
-        }
-    }
+	const handleUserNameChange = e => {
+		const inputVal = e.target.value;
+		if (inputVal.length >= MAX_NAME_LIMIT) {
+			setUserName(inputVal.slice(0, MAX_NAME_LIMIT));
+		} else {
+			setUserName(e.target.value);
+		}
+	};
 
-    const handleNewUserSubmit = (e) => {
-        e.preventDefault();
-        const formattedName = userName.trim();
-        if (formattedName.length === 0) {
-            dispatch(setError({
-                errType: ERR_TYPE.ERR,
-                message: 'Username is invalid. Name must contain at least one character.',
-            }));
-            return;
-        }
-        if (formattedName.length > MAX_NAME_LIMIT) {
-            dispatch(setError({
-                errType: ERR_TYPE.ERR,
-                message: `Username is invalid. Name must not exceed ${MAX_NAME_LIMIT} characters.`,
-            }));
-            return;
-        }
-        if (userName.match(NAME_CHECK_REGEX)) {
-            dispatch(setError({
-                errType: ERR_TYPE.ERR,
-                message: 'Username is invalid. Invalid characters detected. Please use only alphanumeric characters.',
-            }));
-            return;
-        }
-        if (users.includes(formattedName)) {
-            dispatch(setError({
-                errType: ERR_TYPE.ERR,
-                message: 'Username is taken, please submit a unique username.',
-            }));
-            return;
-        }
-        dispatch(resetError());
-        dispatch(addUser(formattedName));
-        setUserName('');
-    }
+	const handleNewUserSubmit = e => {
+		e.preventDefault();
+		const newUser = userName.trim();
+		if (newUser.length === 0) {
+			dispatch(
+				setError({
+					errType: ERR_TYPE.ERR,
+					message:
+						'Username is invalid. Name must contain at least one character.',
+				})
+			);
+			return;
+		}
+		if (newUser.length > MAX_NAME_LIMIT) {
+			dispatch(
+				setError({
+					errType: ERR_TYPE.ERR,
+					message: `Username is invalid. Name must not exceed ${MAX_NAME_LIMIT} characters.`,
+				})
+			);
+			return;
+		}
+		if (userName.match(NAME_CHECK_REGEX)) {
+			dispatch(
+				setError({
+					errType: ERR_TYPE.ERR,
+					message:
+						'Username is invalid. Invalid characters detected. Please use only alphanumeric characters.',
+				})
+			);
+			return;
+		}
+		if (users.includes(newUser)) {
+			dispatch(
+				setError({
+					errType: ERR_TYPE.ERR,
+					message: 'Username is taken, please submit a unique username.',
+				})
+			);
+			return;
+		}
+		dispatch(resetError());
+		// console.log({ newUser });
+		console.log({ planId, newUser });
+		dispatch(addUserAsync({ newUser, planId }));
+		setUserName('');
+	};
 
-    return (
-        <>
-            <Form onSubmit={handleNewUserSubmit}>
-                <Form.Label>Not There? Create a new User</Form.Label>
-                <div>
-                    <input
-                        style={{width: '100%', height: '40px'}}
-                        type='text'
-                        placeholder='Name of new user'
-                        value={userName}
-                        onChange={handleUserNameChange}/>
-                </div>
-                <div>
-                    <Button className='mt-3' type='submit' style={{width: '100%'}}>
-                        Submit
-                    </Button>
-                </div>
-            </Form>
-        </>
-    );
+	return (
+		<>
+			<Form onSubmit={handleNewUserSubmit}>
+				<Form.Label>Not There? Create a new User</Form.Label>
+				<div>
+					<input
+						style={{ width: '100%', height: '40px' }}
+						type="text"
+						placeholder="Name of new user"
+						value={userName}
+						onChange={handleUserNameChange}
+					/>
+				</div>
+				<div>
+					<Button className="mt-3" type="submit" style={{ width: '100%' }}>
+						Submit
+					</Button>
+				</div>
+			</Form>
+		</>
+	);
 };
 
 export default AddUserForm;
--- pro-planner-client/src/redux/userSlice.js
+++ pro-planner-client/src/redux/userSlice.js
@@ -1,23 +1,58 @@
-import {createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { LOAD_STATUS } from '../constants';
+import { buildServerRoute } from '../helpers/Utils';
 
-const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-        userList: ['user1'],
-        selectedUser: null,
-    },
-    reducers: {
-        addUser: (state, action) => {
-            state.userList.push(action.payload);
-        },
-        selectUser: (state, action) => {
-            state.selectedUser = action.payload;
-        },
-        resetUser: (state) => {
-            state.selectedUser = null;
-        }
-    }
+export const getUserAsync = createAsyncThunk('user/get', async ({ planId }) => {
+	const response = await axios.get(buildServerRoute(`user?eventId=${planId}`));
+	// console.log('got the response!', response.data);
+	return response.data;
 });
 
-export const {addUser, selectUser, resetUser} = userSlice.actions;
+export const addUserAsync = createAsyncThunk(
+	'user/add',
+	async ({ planId, newUser }) => {
+		const response = await axios.put(buildServerRoute('user'), {
+			eventId: planId,
+			userName: newUser,
+		});
+		return newUser;
+	}
+);
+
+// export const addUserAsync = createAsyncThunk()
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: {
+		userList: [],
+		selectedUser: null,
+	},
+	reducers: {
+		addUser: (state, action) => {
+			state.userList.push(action.payload);
+		},
+		selectUser: (state, action) => {
+			state.selectedUser = action.payload;
+		},
+		resetUser: state => {
+			state.selectedUser = null;
+		},
+	},
+	extraReducers: builder => {
+		/* TODO: Complete the pending and rejected cases */
+		// builder.addCase(getUserAsync.pending, state => {});
+		builder.addCase(getUserAsync.fulfilled, (state, action) => {
+			state.userList = action.payload;
+		});
+		builder.addCase(addUserAsync.fulfilled, (state, action) => {
+			state.userList.push(action.payload);
+		});
+		// builder.addCase(getUserAsync.rejected, state => {
+		// 	console.log('error');
+		// });
+	},
+});
+
+export const { addUser, selectUser, resetUser } = userSlice.actions;
 export default userSlice.reducer;
