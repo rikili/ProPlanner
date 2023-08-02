b637e1e42802ac9d19b07fba6b23a2a9f710cf1c
Parham Alijani <palijani@students.cs.ubc.ca>
2023-08-01 18:26:25 -0700

    error handling and cleanup

--- pro-planner-client/src/redux/userSlice.js
+++ pro-planner-client/src/redux/userSlice.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import { LOAD_STATUS } from '../constants';
+import { LOAD_STATUS } from '../constants';
 import { buildServerRoute } from '../helpers/Utils';
 
 export const getUserAsync = createAsyncThunk('user/get', async ({ planId }) => {
 	const response = await axios.get(buildServerRoute(`user?eventId=${planId}`));
-	// console.log('got the response!', response.data);
 	return response.data;
 });
 
 export const addUserAsync = createAsyncThunk(
 	'user/add',
 	async ({ planId, newUser }) => {
-		const response = await axios.put(buildServerRoute('user'), {
+		await axios.put(buildServerRoute('user'), {
 			eventId: planId,
 			userName: newUser,
 		});
@@ -20,13 +19,12 @@ export const addUserAsync = createAsyncThunk(
 	}
 );
 
-// export const addUserAsync = createAsyncThunk()
-
 const userSlice = createSlice({
 	name: 'user',
 	initialState: {
 		userList: [],
 		selectedUser: null,
+		loadStatus: null,
 	},
 	reducers: {
 		addUser: (state, action) => {
@@ -40,17 +38,26 @@ const userSlice = createSlice({
 		},
 	},
 	extraReducers: builder => {
-		/* TODO: Complete the pending and rejected cases */
-		// builder.addCase(getUserAsync.pending, state => {});
+		builder.addCase(getUserAsync.pending, state => {
+			state.pollStatus = LOAD_STATUS.LOADING;
+		});
 		builder.addCase(getUserAsync.fulfilled, (state, action) => {
 			state.userList = action.payload;
+			state.pollStatus = LOAD_STATUS.SUCCESS;
+		});
+		builder.addCase(getUserAsync.rejected, state => {
+			state.pollStatus = LOAD_STATUS.FAILED;
+		});
+		builder.addCase(addUserAsync.pending, (state, action) => {
+			state.pollStatus = LOAD_STATUS.LOADING;
 		});
 		builder.addCase(addUserAsync.fulfilled, (state, action) => {
 			state.userList.push(action.payload);
+			state.pollStatus = LOAD_STATUS.SUCCESS;
 		});
-		// builder.addCase(getUserAsync.rejected, state => {
-		// 	console.log('error');
-		// });
+		builder.addCase(addUserAsync.rejected, (state, action) => {
+			state.pollStatus = LOAD_STATUS.FAILED;
+		});
 	},
 });
 
