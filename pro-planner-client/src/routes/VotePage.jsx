922119d757a299555bbd48142778ad93661c7834
Heeseop An <heeseop.an@gmail.com>
2023-08-02 21:51:18 -0700

    update tripId to planId for frontend

--- pro-planner-client/src/routes/VotePage.jsx
+++ pro-planner-client/src/routes/VotePage.jsx
@@ -3,22 +3,28 @@ import AddPollForm from "../components/AddPollForm";
 import Polls from "../components/Polls";
 import {getPollAsync} from "../redux/pollSlice";
 import {useDispatch, useSelector} from "react-redux";
+import {LOAD_STATUS} from "../constants";
+import LoadingDisplay from "../components/LoadingDisplay";
 import {useLocation} from "react-router";
 
 const VotePage = () => {
-    const tripId = useLocation().pathname.split('/')[1];
+
+    // const planId = '64c5cbf8b6cdc4ef3c78be6a'; // testing purpose
+    const planId = useLocation().pathname.split('/')[1];
     const polls = useSelector((state) => state.poll.polls);
+    const loadingState = useSelector((state) => state.poll.pollStatus);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getPollAsync({tripId}))
+        dispatch(getPollAsync({planId}))
     }, [dispatch])
 
 
-    return <div>
+    return <>
+        {(loadingState === LOAD_STATUS.LOADING) && <LoadingDisplay/>}
         <AddPollForm polls={polls}/>
         <Polls polls={polls}/>
-    </div>
+    </>
 };
 
 export default VotePage;
