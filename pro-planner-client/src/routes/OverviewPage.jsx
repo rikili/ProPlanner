269620ccc5c44a37a26ff33c694211b73bda1320
Heeseop An <heeseop.an@gmail.com>
2023-08-02 20:46:09 -0700

    add date processing function

--- pro-planner-client/src/routes/OverviewPage.jsx
+++ pro-planner-client/src/routes/OverviewPage.jsx
@@ -5,14 +5,30 @@ import './OverviewPage.scss';
 import ExpenseSplit from '../components/ExpenseSplit';
 import {useSelector} from 'react-redux';
 import Map from '../components/Map';
+import {format, parseISO} from "date-fns";
 
 const OverviewPage = () => {
-    // const tripName = useSelector((state) => state.planParameters.name);
-    // const location = useSelector((state) => state.planParameters.location);
-    const decidedDates = useSelector(state => state.planParameters.decisionRange); // TODO: process decidedDates by plan type
-    const tripName = 'Trip Name'; // testing purpose
-    const location = 'Vancouver'; // testing purpose
-    const description = 'Bring an umbrella'; // testing purpose
+    const tripName = useSelector((state) => state.planParameters.name);
+    const location = useSelector((state) => state.planParameters.location);
+    const decidedDates = useSelector(state => state.planParameters.decisionRange);
+    const description = useSelector(state => state.planParameters.description);
+    // const decidedDates = ["2023-07-31T07:00:00.000Z", "2023-08-04T06:59:59.099Z"] // testing purpose
+    // const tripName = 'Trip Name'; // testing purpose
+    // const location = 'Vancouver'; // testing purpose
+    // const description = "description test"; // testing purpose
+
+
+    function processDecidedDates(decidedDates) {
+        if (!decidedDates || decidedDates.length !== 2) {
+            return "Dates are not set"
+        }
+        const [startDate, endDate] = decidedDates;
+        const processedStartDate = format(parseISO(startDate), 'EEE, MMM d yyyy');
+        const processedEndDate = format(parseISO(endDate), 'EEE, MMM d yyyy');
+        return `${processedStartDate} - ${processedEndDate}`
+    }
+
+    const processedDecidedDates = processDecidedDates(decidedDates);
 
     return (
         <>
@@ -30,14 +46,11 @@ const OverviewPage = () => {
                             <Card.Body>
                                 <Card.Title>Details</Card.Title>
                                 <Card.Subtitle>Dates:</Card.Subtitle>
-                                <Card.Text>
-                                    {/*TODO: process decidedDates by plan type */}
-                                    {decidedDates.length > 0 ? decidedDates : `Dates not set`}
-                                </Card.Text>
+                                <Card.Text>{processedDecidedDates}</Card.Text>
                                 <Card.Subtitle>Location:</Card.Subtitle>
                                 <Card.Text>{location}</Card.Text>
-                                <Card.Subtitle>Description:</Card.Subtitle>
-                                <Card.Text>{description}</Card.Text>
+                                {description ? <Card.Subtitle>Description:</Card.Subtitle> : null}
+                                {description ? <Card.Text>{description}</Card.Text> : null}
                             </Card.Body>
                         </Card>
                     </Col>
@@ -56,4 +69,5 @@ const OverviewPage = () => {
 };
 
 export default OverviewPage;
+
 
