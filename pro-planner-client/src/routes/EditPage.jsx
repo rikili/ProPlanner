80a6ac09bb10bae18aac020946e80fea0a61176d
R <rikli258@gmail.com>
2023-08-04 15:07:44 -0700

    clear decision when cut by edit

--- pro-planner-client/src/routes/EditPage.jsx
+++ pro-planner-client/src/routes/EditPage.jsx
@@ -3,19 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { buildServerRoute } from '../helpers/Utils';
 import { setError } from '../redux/errorSlice';
-import { ERR_TYPE } from '../constants';
+import { ERR_TYPE, PLAN_TYPE } from '../constants';
 import { updatePlan } from '../redux/planParamSlice';
 import axios from 'axios';
 import LoadingDisplay from '../components/LoadingDisplay';
 import ParameterForm from '../components/ParameterForm';
+import { isAfter, isBefore } from 'date-fns';
 
 const EditPage = () => {
     const [isUploading, setIsUploading] = useState(false);
     const { tripId } = useParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    const decision = useSelector(state => state.planParameters.decisionRange);
     const params = useSelector((state) => state.planParameters);
+
+    const checkDecisionInRange = (dateRange, planType) => {
+        if (!decision.length) return false;
+        const startTime = (planType === PLAN_TYPE.OUTING) ? dateRange[0][0] : dateRange[0];
+        const endTime = dateRange[1];
+        return isBefore(new Date(decision[0]), new Date(startTime)) || isAfter(new Date(decision[1]), new Date(endTime));
+    }
 
     const existingData = {
         name: params.name,
@@ -29,8 +37,13 @@ const EditPage = () => {
     };
 
     const handleSubmission = (formResult, planType) => {
+        let shouldClearDecision = false;
+        if (checkDecisionInRange(formResult.dateTimeRange, planType)) {
+            shouldClearDecision = true;
+        }
+
         axios
-            .put(buildServerRoute('plan', tripId), { ...formResult, planType })
+            .put(buildServerRoute('plan', tripId), { ...formResult, planType, decision: shouldClearDecision ? [] : decision })
             .then((result) => {
                 dispatch(updatePlan(result.data));
                 setIsUploading(false);
