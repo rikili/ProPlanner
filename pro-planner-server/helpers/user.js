26d162eecb9761273203cec28ae6ec47827af79f
palijanii <parham360321@gmail.com>
2023-08-04 01:44:13 -0700

    deleting users integrated

--- pro-planner-server/helpers/user.js
+++ pro-planner-server/helpers/user.js
@@ -4,39 +4,59 @@
 const { ObjectId } = require('mongodb');
 const plan = require('../models/plan');
 
-async function addUser(eventId, userName) {
-  let users = await plan.findOneAndUpdate(
-    { _id: new ObjectId(eventId) },
-    {
-      $set: {
-        [`userInfo.${userName}`]: { id: new ObjectId() },
-      },
-    },
-    {
-      new: true,
-      projection: {
-        _id: 0,
-        userInfo: 1,
-      },
-    }
-  );
-  users = users.toJSON().userInfo;
-  return users;
+async function addUser(planId, userName) {
+	let users = await plan.findOneAndUpdate(
+		{ _id: new ObjectId(planId) },
+		{
+			$set: {
+				[`userInfo.${userName}`]: { id: new ObjectId() },
+			},
+		},
+		{
+			new: true,
+			projection: {
+				_id: 0,
+				userInfo: 1,
+			},
+		}
+	);
+	// users = users.toJSON().userInfo;
+	// return users;
 }
 
-async function getUsers(eventId) {
-  let users = await plan.findOne(
-    { _id: new ObjectId(eventId) },
-    {
-      _id: 0,
-      userInfo: 1,
-    }
-  );
-  users = users.toJSON().userInfo;
-  return users;
+async function deleteUser(planId, userName) {
+	let user = await plan.findOneAndUpdate(
+		{ _id: new ObjectId(planId) },
+		{
+			$unset: {
+				[`userInfo.${userName}`]: 1,
+			},
+		},
+		{
+			new: true,
+			projection: {
+				_id: 0,
+				userInfo: 1,
+			},
+		}
+	);
+	return user.toJSON().userInfo;
+}
+
+async function getUsers(planId) {
+	let users = await plan.findOne(
+		{ _id: new ObjectId(planId) },
+		{
+			_id: 0,
+			userInfo: 1,
+		}
+	);
+	users = users.toJSON().userInfo;
+	return users;
 }
 
 module.exports = {
-  addUser,
-  getUsers,
+	addUser,
+	deleteUser,
+	getUsers,
 };
