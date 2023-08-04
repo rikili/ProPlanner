64513437fb72722cb30c54984c35d29361e54693
Heeseop An <heeseop.an@gmail.com>
2023-08-04 15:44:33 -0700

    accept plan url + add error handling for landing page

--- pro-planner-client/src/routes/LandingPage.jsx
+++ pro-planner-client/src/routes/LandingPage.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useRef } from 'react';
+// url validation reference: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+// url extraction reference: https://developer.mozilla.org/en-US/docs/Web/API/URL#Properties
+import React, {useState, useRef} from 'react';
 import {
     Button,
     Card,
@@ -6,141 +8,217 @@ import {
     Container,
     Form,
     Overlay,
-    OverlayTrigger,
     Row,
     Tooltip
 } from 'react-bootstrap';
-import { FiInfo } from 'react-icons/fi';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-
-import { changePlanType } from '../redux/planParamSlice';
-import { PLAN_TYPE } from '../constants';
+import {FiInfo} from 'react-icons/fi';
+import {useNavigate} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {changePlanType} from '../redux/planParamSlice';
+import {ERR_TYPE, PLAN_TYPE} from '../constants';
+import {setError} from "../redux/errorSlice";
+import axios from "axios";
+import {buildServerRoute} from "../helpers/Utils";
 
 const LandingPage = () => {
-    const [inputValue, setInputValue] = useState('');
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+        const [inputValue, setInputValue] = useState('');
+        const dispatch = useDispatch();
+        const navigate = useNavigate();
 
-    const tripButtonIcon = useRef(null);
-    const outingButtonIcon = useRef(null);
+        const tripButtonIcon = useRef(null);
+        const outingButtonIcon = useRef(null);
 
-    const [showTripTooltip, setShowTripTooltip] = useState(false);
-    const [showOutingTooltip, setShowOutingTooltip] = useState(false);
+        const [showTripTooltip, setShowTripTooltip] = useState(false);
+        const [showOutingTooltip, setShowOutingTooltip] = useState(false);
+        // may remove the localhost regex once deployed
+        const planUrlRegex = /^((https?:\/\/[^/]+\/[^/]+)|(https?:\/\/localhost:3000\/[^/]+))$/;
 
-    const handleInput = e => {
-        setInputValue(e.target.value);
-    };
+        const handleInput = e => {
+            setInputValue(e.target.value);
+        };
 
-    const handleJoin = (event) => {
-        event.preventDefault();
-        navigate(`/user/${inputValue}`);
-    }
+        const isValidURL = (url) => {
+            try {
+                new URL(url);
+                return true;
+            } catch {
+                return false;
+            }
+        }
 
-    const handleCreate = (planType) => {
-        dispatch(changePlanType(planType));
-        navigate(`create`);
-    }
+        const isValidPlanURL = (url) => {
+            return planUrlRegex.test(url)
+        }
 
-    return (
-        <Container>
-            <Row className="vh-100 d-flex justify-content-center align-items-center">
-                <Col md={8} lg={6} xs={12}>
-                    <div className="border border-3 border-primary" style={{position: 'relative', top: '0.3em', zIndex: '1'}} />
-                    <Card className="shadow">
-                        <Card.Body>
-                            <div className="mt-md-4">
-                                <h2 className="fw-bold text-uppercase d-flex justify-content-center align-items-center">
-                                    ProPlanner
-                                </h2>
-                                <p className="d-flex justify-content-center align-items-center">
-                                    Choose the Type of Plan you'd like to Create
-                                </p>
-                                <div className="mb-3">
-                                    <Row className="text-center mb-3">
-                                        <Button
-                                            variant="primary"
-                                            size="lg"
-                                            className="mb-2 w-50 m-auto d-flex"
-                                            onClick={() => handleCreate(PLAN_TYPE.TRIP)}
-                                        >
-                                            <div className="m-auto">Trip</div>
-                                            <span className="float-end"
-                                                  onMouseEnter={() => setShowTripTooltip(true)}
-                                                  onMouseLeave={() => setShowTripTooltip(false)}
-                                                  ref={tripButtonIcon}>
-                                                <FiInfo />
-                                            </span>
-                                            <Overlay
-                                                target={tripButtonIcon.current}
-                                                show={showTripTooltip}
-                                                placement="top"
+        const getPlanId = (value) => {
+            return new URL(value).pathname.substring(1);
+        }
+
+        const handleJoin = (event) => {
+            event.preventDefault();
+
+            if (!inputValue) {
+                dispatch(
+                    setError({
+                        errType: ERR_TYPE.ERR,
+                        message:
+                            "Please enter a URL or Code.",
+                    })
+                );
+                return;
+            }
+
+            if (isValidURL(inputValue) && !isValidPlanURL(inputValue)) {
+                dispatch(
+                    setError({
+                        errType: ERR_TYPE.ERR,
+                        message:
+                            "Invalid URL format: example of a valid URL format is 'https://proplanner.onrender.com/PLAN_ID'.",
+                    })
+                );
+                return;
+            }
+
+
+            if (isValidURL(inputValue) && isValidPlanURL(inputValue)) {
+                let planId = getPlanId(inputValue);
+                axios.get(buildServerRoute('plan', planId))
+                    .then(() => {
+                        navigate(`/user/${planId}`);
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 404) {
+                            dispatch(setError({
+                                errType: ERR_TYPE.ERR,
+                                message: 'Plan does not exist, please enter an existing Plan URL.',
+                            }));
+                        }
+                    });
+
+            }
+
+            if (!isValidURL(inputValue)) {
+                axios.get(buildServerRoute('plan', inputValue))
+                    .then(() => {
+                        navigate(`/user/${inputValue}`);
+                    })
+                    .catch((err) => {
+                        if (err.response.status === 404) {
+                            dispatch(setError({
+                                errType: ERR_TYPE.ERR,
+                                message: 'Plan does not exist, please enter an existing Plan Code.',
+                            }));
+                        }
+                    });
+            }
+        }
+
+        const handleCreate = (planType) => {
+            dispatch(changePlanType(planType));
+            navigate(`create`);
+        }
+
+        return (
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center align-items-center">
+                    <Col md={8} lg={6} xs={12}>
+                        <div className="border border-3 border-primary"
+                             style={{position: 'relative', top: '0.3em', zIndex: '1'}}/>
+                        <Card className="shadow">
+                            <Card.Body>
+                                <div className="mt-md-4">
+                                    <h2 className="fw-bold text-uppercase d-flex justify-content-center align-items-center">
+                                        ProPlanner
+                                    </h2>
+                                    <p className="d-flex justify-content-center align-items-center">
+                                        Choose the Type of Plan you'd like to Create
+                                    </p>
+                                    <div className="mb-3">
+                                        <Row className="text-center mb-3">
+                                            <Button
+                                                variant="primary"
+                                                size="lg"
+                                                className="mb-2 w-50 m-auto d-flex"
+                                                onClick={() => handleCreate(PLAN_TYPE.TRIP)}
                                             >
-                                                {(props) => (
-                                                    <Tooltip {...props}>
-                                                        {'Scheduling based on scale of half day increments. For planning longer trips/timeframes'}
-                                                    </Tooltip>
-                                                )}
-                                            </Overlay>
-                                        </Button>
-                                    </Row>
-                                    <Row className="text-center mb-3">
-                                        <Button
-                                            variant="primary"
-                                            size="lg"
-                                            className="w-50 m-auto d-flex"
-                                            onClick={() => handleCreate(PLAN_TYPE.OUTING)}
-                                        >
-                                            <div className="m-auto">Outing</div>
-                                            <span className="float-end"
-                                                  onMouseEnter={() => setShowOutingTooltip(true)}
-                                                  onMouseLeave={() => setShowOutingTooltip(false)}
-                                                  ref={outingButtonIcon}>
-                                                <FiInfo />
+                                                <div className="m-auto">Trip</div>
+                                                <span className="float-end"
+                                                      onMouseEnter={() => setShowTripTooltip(true)}
+                                                      onMouseLeave={() => setShowTripTooltip(false)}
+                                                      ref={tripButtonIcon}>
+                                                <FiInfo/>
                                             </span>
-                                            <Overlay
-                                                target={outingButtonIcon.current}
-                                                show={showOutingTooltip}
-                                                placement="bottom"
+                                                <Overlay
+                                                    target={tripButtonIcon.current}
+                                                    show={showTripTooltip}
+                                                    placement="top"
+                                                >
+                                                    {(props) => (
+                                                        <Tooltip {...props}>
+                                                            {'Scheduling based on scale of half day increments. For planning longer trips/timeframes'}
+                                                        </Tooltip>
+                                                    )}
+                                                </Overlay>
+                                            </Button>
+                                        </Row>
+                                        <Row className="text-center mb-3">
+                                            <Button
+                                                variant="primary"
+                                                size="lg"
+                                                className="w-50 m-auto d-flex"
+                                                onClick={() => handleCreate(PLAN_TYPE.OUTING)}
                                             >
-                                                {(props) => (
-                                                    <Tooltip {...props}>
-                                                        {'Scheduling based on scale of half hour increments. For planning a one-day event'}
-                                                    </Tooltip>
-                                                )}
-                                            </Overlay>
-                                        </Button>
-                                    </Row>
-                                    <div className="mt-3">
-                                        <div className="text-center">
-                                            Joining a Plan? Enter the URL or Code.
-                                            <Form className="join-form">
-                                                <Form.Control
-                                                    placeholder="Enter URL/Code"
-                                                    value={inputValue}
-                                                    onChange={handleInput}
-                                                />
-                                                <div className="m-auto align-middle text-center mt-4">
-                                                    <Button
-                                                        variant="primary"
-                                                        type="submit"
-                                                        className="mt-2"
-                                                        onClick={handleJoin}
-                                                    >
-                                                        Join
-                                                    </Button>
-                                                </div>
-                                            </Form>
+                                                <div className="m-auto">Outing</div>
+                                                <span className="float-end"
+                                                      onMouseEnter={() => setShowOutingTooltip(true)}
+                                                      onMouseLeave={() => setShowOutingTooltip(false)}
+                                                      ref={outingButtonIcon}>
+                                                <FiInfo/>
+                                            </span>
+                                                <Overlay
+                                                    target={outingButtonIcon.current}
+                                                    show={showOutingTooltip}
+                                                    placement="bottom"
+                                                >
+                                                    {(props) => (
+                                                        <Tooltip {...props}>
+                                                            {'Scheduling based on scale of half hour increments. For planning a one-day event'}
+                                                        </Tooltip>
+                                                    )}
+                                                </Overlay>
+                                            </Button>
+                                        </Row>
+                                        <div className="mt-3">
+                                            <div className="text-center">
+                                                Joining a Plan? Enter the URL or Code.
+                                                <Form className="join-form">
+                                                    <Form.Control
+                                                        placeholder="Enter URL/Code"
+                                                        value={inputValue}
+                                                        onChange={handleInput}
+                                                    />
+                                                    <div className="m-auto align-middle text-center mt-4">
+                                                        <Button
+                                                            variant="primary"
+                                                            type="submit"
+                                                            className="mt-2"
+                                                            onClick={handleJoin}
+                                                        >
+                                                            Join
+                                                        </Button>
+                                                    </div>
+                                                </Form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    );
-};
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+;
 
 export default LandingPage;
