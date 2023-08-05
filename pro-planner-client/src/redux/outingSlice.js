09a1ca64d87ba69be37800e5015a86a92d2176db
Parham Alijani <palijani@students.cs.ubc.ca>
2023-08-05 14:46:17 -0700

    delete users bug fixed

--- pro-planner-client/src/redux/outingSlice.js
+++ pro-planner-client/src/redux/outingSlice.js
@@ -2,67 +2,80 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { buildServerRoute } from '../helpers/Utils';
 
-export const updateOutings = createAsyncThunk('outing/update', async({ user, planId, selections, months }, { rejectWithValue }) => {
-    const promises = months.map((monthIndex) => axios.put(buildServerRoute('outing', planId), {
-        userId: user,
-        selections: {
-            [monthIndex]: selections[monthIndex]
-        }
-    }));
-    let reject = false;
-    const result = (await Promise.allSettled(promises)).map((res) => {
-        if (!res.reason) return res.value.data.month;
-        reject = true;
-        return null; // clear out error cases
-    });
-    const payload = {
-        result,
-        months,
-        user,
-    };
-    return (reject ? rejectWithValue(payload) : payload);
-});
+export const updateOutings = createAsyncThunk(
+	'outing/update',
+	async ({ user, planId, selections, months }, { rejectWithValue }) => {
+		const promises = months.map(monthIndex =>
+			axios.put(buildServerRoute('outing', planId), {
+				userId: user,
+				selections: {
+					[monthIndex]: selections[monthIndex],
+				},
+			})
+		);
+		let reject = false;
+		const result = (await Promise.allSettled(promises)).map(res => {
+			if (!res.reason) return res.value.data.month;
+			reject = true;
+			return null; // clear out error cases
+		});
+		const payload = {
+			result,
+			months,
+			user,
+		};
+		return reject ? rejectWithValue(payload) : payload;
+	}
+);
 
 const outingSlice = createSlice({
 	name: 'outing',
 	initialState: {
-        isLoading: false,
-        selections: {},
-    },
+		isLoading: false,
+		selections: {},
+	},
 	reducers: {
 		setUserSelections(state, action) {
 			const { user, selections } = action.payload;
 			state.selections[user] = selections;
 		},
-        setLoading(state, { payload }) {
-            state.isLoading = payload;
-        },
-        resetUpdateFailed(state) {
-            state.updateFailed = false;
-        }
+		setLoading(state, { payload }) {
+			state.isLoading = payload;
+		},
+		resetUpdateFailed(state) {
+			state.updateFailed = false;
+		},
+		clearOutingSelections(state) {
+			state.selections = {};
+		},
 	},
-    extraReducers: (builder) => {
-        builder.addCase(updateOutings.fulfilled, (state, { payload }) => {
-            const { result, hasUpdateFailed, months, user } = payload;
+	extraReducers: builder => {
+		builder.addCase(updateOutings.fulfilled, (state, { payload }) => {
+			const { result, hasUpdateFailed, months, user } = payload;
 
-            months.forEach((monthIndex, index) => {
-                const userSelections = state.selections[user];
-                const monthResult = result[index];
-                userSelections[monthIndex] = monthResult;
-            });
-        });
-        builder.addCase(updateOutings.rejected, (state, { payload }) => {
-            const { result, months, user } = payload;
+			months.forEach((monthIndex, index) => {
+				const userSelections = state.selections[user];
+				const monthResult = result[index];
+				userSelections[monthIndex] = monthResult;
+			});
+		});
+		builder.addCase(updateOutings.rejected, (state, { payload }) => {
+			const { result, months, user } = payload;
 
-            months.forEach((monthIndex, index) => {
-                const userSelections = state.selections[user];
-                const monthResult = result[index];
-                if (!monthResult) return;
-                userSelections[monthIndex] = monthResult;
-            });
-        });
-    }
+			months.forEach((monthIndex, index) => {
+				const userSelections = state.selections[user];
+				const monthResult = result[index];
+				if (!monthResult) return;
+				userSelections[monthIndex] = monthResult;
+			});
+		});
+	},
 });
 
-export const { setUserSelections, setLoading, resetUpdateFailed } = outingSlice.actions;
+export const {
+	setUserSelections,
+	setLoading,
+	resetUpdateFailed,
+	clearOutingSelections,
+} = outingSlice.actions;
 export default outingSlice.reducer;
--- pro-planner-client/src/redux/tripSlice.js
+++ pro-planner-client/src/redux/tripSlice.js
@@ -3,90 +3,99 @@ import { buildServerRoute, getTimezone } from '../helpers/Utils';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const setUserSelectionsAsync = createAsyncThunk(
-    'trip/updateSelects',
-    async ({ tripId, userId, newSelections, months }, { rejectWithValue }) => {
-        const timezone = getTimezone();
+	'trip/updateSelects',
+	async ({ tripId, userId, newSelections, months }, { rejectWithValue }) => {
+		const timezone = getTimezone();
 
-        const promises = months.map((monthIndex) =>
-            axios.put(buildServerRoute('trip', tripId), {
-                userId,
-                timezone,
-                selections: {
-                    [monthIndex]: newSelections[monthIndex],
-                },
-            })
-        );
+		const promises = months.map(monthIndex =>
+			axios.put(buildServerRoute('trip', tripId), {
+				userId,
+				timezone,
+				selections: {
+					[monthIndex]: newSelections[monthIndex],
+				},
+			})
+		);
 
-        const results = await Promise.allSettled(promises);
+		const results = await Promise.allSettled(promises);
 
-        let reject = false;
-        const result = results.map((res) => {
-            if (!res.reason) return res;
-            reject = true;
-            return null;
-        });
+		let reject = false;
+		const result = results.map(res => {
+			if (!res.reason) return res;
+			reject = true;
+			return null;
+		});
 
-        const payloadData = {};
+		const payloadData = {};
 
-        result.forEach((res, index) => {
-            if (!res) return;
-            const monthIndex = months[index];
-            payloadData[monthIndex] = res.value.data.month;
-        });
+		result.forEach((res, index) => {
+			if (!res) return;
+			const monthIndex = months[index];
+			payloadData[monthIndex] = res.value.data.month;
+		});
 
-        const payload = {
-            userId: userId,
-            data: payloadData,
-        }
-        return (reject ? rejectWithValue(payload) : payload);
-    }
+		const payload = {
+			userId: userId,
+			data: payloadData,
+		};
+		return reject ? rejectWithValue(payload) : payload;
+	}
 );
 
 const addSelectionToUser = (state, userId, monthIndex, monthSelects) => {
-    if (!state.selections[userId]) {
-        state.selections[userId] = { [monthIndex]: monthSelects };
-    } else {
-        state.selections[userId][monthIndex] = monthSelects;
-    }
+	if (!state.selections[userId]) {
+		state.selections[userId] = { [monthIndex]: monthSelects };
+	} else {
+		state.selections[userId][monthIndex] = monthSelects;
+	}
 };
 
 const tripSlice = createSlice({
-    name: 'trip',
-    initialState: {
-        isLoading: false, // in loading state
-        isInitDone: false, // inital loading finished for user fetching
-        selections: {},
-    },
-    reducers: {
-        setUserSelections(state, action) {
-            const { userId, monthIndex, data } = action.payload;
-            if (!state.selections[userId]) {
-                state.selections[userId] = {};
-            }
-            state.selections[userId][monthIndex] = data;
-        },
-        setLoading(state, action) {
-            state.isLoading = action.payload;
-        },
-        completeInit(state) {
-            state.isInitDone = true;
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(setUserSelectionsAsync.fulfilled, (state, action) => {
-            const { userId, data } = action.payload;
-            Object.entries(data).forEach(([monthIndex, selections]) => {
-                addSelectionToUser(state, userId, monthIndex, selections);
-            });
-        });
-        builder.addCase(setUserSelectionsAsync.rejected, (state, action) => {
-            const { userId, data } = action.payload;
-            Object.entries(data).forEach(([monthIndex, selections]) => {
-                addSelectionToUser(state, userId, monthIndex, selections);
-            });
-        });
-    },
+	name: 'trip',
+	initialState: {
+		isLoading: false, // in loading state
+		isInitDone: false, // inital loading finished for user fetching
+		selections: {},
+	},
+	reducers: {
+		setUserSelections(state, action) {
+			const { userId, monthIndex, data } = action.payload;
+			if (!state.selections[userId]) {
+				state.selections[userId] = {};
+			}
+			state.selections[userId][monthIndex] = data;
+		},
+		setLoading(state, action) {
+			state.isLoading = action.payload;
+		},
+		completeInit(state) {
+			state.isInitDone = true;
+		},
+
+		clearTripSelections(state, action) {
+			state.selections = {};
+		},
+	},
+	extraReducers: builder => {
+		builder.addCase(setUserSelectionsAsync.fulfilled, (state, action) => {
+			const { userId, data } = action.payload;
+			Object.entries(data).forEach(([monthIndex, selections]) => {
+				addSelectionToUser(state, userId, monthIndex, selections);
+			});
+		});
+		builder.addCase(setUserSelectionsAsync.rejected, (state, action) => {
+			const { userId, data } = action.payload;
+			Object.entries(data).forEach(([monthIndex, selections]) => {
+				addSelectionToUser(state, userId, monthIndex, selections);
+			});
+		});
+	},
 });
 
-export const { setUserSelections, setLoading, completeInit } = tripSlice.actions;
+export const {
+	setUserSelections,
+	setLoading,
+	completeInit,
+	clearTripSelections,
+} = tripSlice.actions;
 export default tripSlice.reducer;
