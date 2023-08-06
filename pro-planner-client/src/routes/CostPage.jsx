5ae065a807a98935a9c2dc108e9dac305d7d7ab8
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-08-06 16:44:15 -0700

    cost split card not displaying on overview page

--- pro-planner-client/src/routes/CostPage.jsx
+++ pro-planner-client/src/routes/CostPage.jsx
@@ -1,23 +1,14 @@
 import TripExpense from "../components/TripExpense";
 import ExpenseSplit from "../components/ExpenseSplit";
 import LoadingDisplay from "../components/LoadingDisplay"
-import { getCostAsync } from "../redux/costSlice";
-import { Row, Col, Container } from "react-bootstrap"
-import { useLocation } from "react-router";
-import { useSelector, useDispatch } from "react-redux";
-import React, {useEffect} from "react";
+import { Col, Container } from "react-bootstrap"
+import { useSelector } from "react-redux";
+import React from "react";
 import {LOAD_STATUS} from "../constants";
 import './CostPage.scss';
 
 const CostPage = () => {
-
-    const tripId = useLocation().pathname.split('/')[1];
     const loadingState = useSelector((state) => state.cost.costStatus);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getCostAsync({tripId}))
-    }, [dispatch, tripId])
 
     return (
         <>
