c6cca1b137c0bfc9250bd8cf5d297c6c447691e6
R <rikli258@gmail.com>
2023-08-06 14:27:49 -0700

    minor styling changes

--- pro-planner-client/src/components/override/Button.jsx
+++ pro-planner-client/src/components/override/Button.jsx
@@ -20,6 +20,11 @@ const Button = (props) => {
                     color: white !important;
                     background-color: rgb(22, 55, 94) !important;
                 }
+                .btn-custom-primary:focus {
+                    color: white !important;
+                    background-color: rgb(43, 92, 148) !important;
+                    box-shadow: 0 0 1px 0.1em rgba(9, 108, 181, 0.4) !important;
+                }
                 .btn-custom-primary:focus-visible {
                     color: white !important;
                     background-color: rgb(43, 92, 148) !important;
@@ -50,6 +55,12 @@ const Button = (props) => {
                     color: black;
                     background-color: rgb(182, 182, 182);   
                 }
+                .btn-custom-secondary:focus {
+                    color: black !important;
+                    background-color: rgb(182, 182, 182) !important;
+                    border-color: white !important;
+                    box-shadow: 0 0 1px 0.1em rgba(108, 108, 108, 0.4) !important;
+                }
                 .btn-custom-secondary:disabled {
                     color: grey;
                     background-color: rgb(207, 207, 207);
@@ -69,13 +80,18 @@ const Button = (props) => {
                 }
                 .btn-custom-danger:focus-visible {
                     color: rgb(119, 23, 23) !important;
-                    background-color: rgb(179, 49, 49) !important;
+                    background-color: rgb(209, 54, 54) !important;
+                }
+                .btn-custom-danger:focus {
+                    color: rgb(119, 23, 23) !important;
+                    border-color: white;
+                    background-color: rgb(209, 54, 54) !important;
+                    box-shadow: 0 0 1px 0.1em rgba(181, 20, 9, 0.4) !important;
                 }
                 .btn-custom-danger:hover:active {
                     color: white;
                     background-color: rgb(153, 24, 24);
                 }
-                
                 .btn-custom-danger:disabled {
                     color: rgb(226, 119, 119);
                     border-color: rgb(235, 190, 190);
@@ -102,6 +118,12 @@ const Button = (props) => {
                     color: white !important;
                     background-color: rgb(79, 173, 103) !important;
                 }
+                .btn-custom-success:focus {
+                    color: white !important;
+                    border-color: white !important;
+                    background-color: rgb(79, 173, 103) !important;
+                    box-shadow: 0 0 1px 0.1em rgba(9, 181, 107, 0.4) !important;
+                }
                 .btn-custom-success:disabled {
                     color: rgb(60, 167, 86);
                     border-color: rgb(135, 202, 152);
@@ -123,6 +145,11 @@ const Button = (props) => {
                 .btn-custom-outline-primary:hover:active {
                     color: white;
                     background-color: rgb(22, 55, 94);
+                }
+                .btn-custom-outline-primary:focus {
+                    color: rgb(23, 69, 126);
+                    border-color: rgb(23, 69, 126);
+                    box-shadow: 0 0 1px 0.2em rgba(9, 108, 181, 0.4) !important;
                 }
             `}
         </style>
