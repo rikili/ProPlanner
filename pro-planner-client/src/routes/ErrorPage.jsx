2d00af65a2fc45b76dbd66fa799ac49b58508b52
R <rikli258@gmail.com>
2023-08-06 17:46:19 -0700

    stylize error page + add redirect to landing

--- pro-planner-client/src/routes/ErrorPage.jsx
+++ pro-planner-client/src/routes/ErrorPage.jsx
@@ -1,12 +1,33 @@
-import { useRouteError } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
+let timeoutTrack;
 const ErrorPage = () => {
     const error = useRouteError();
-    console.error(error);
+    const navigate = useNavigate();
 
-    return <p>
-        <b>404 Page Not Found.</b> There seems to be an error with the URL path.
-    </p>
-}
+    useEffect(() => {
+        if (timeoutTrack) {
+            clearTimeout(timeoutTrack);
+            timeoutTrack = null;
+        }
+        timeoutTrack = setTimeout(() => {
+            timeoutTrack = null;
+            navigate('/');
+        }, 5000);
+    }, []);
+
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100 flex-column">
+            <h1 className="mb-3" style={{color: "rgb(23, 69, 126)"}}>ProPlanner</h1>
+            <p className="text-center" style={{fontSize: '1.3em'}}>
+                <b>{`${error.status} ${error.statusText}.`}</b>
+                <br />
+                Seems like you've ended up somewhere you shouldn't be<br/>
+            </p>
+            <p style={{color: 'grey'}}>You will be redirected shortly. . .</p>
+        </div>
+    );
+};
 
 export default ErrorPage;
