f91ca106f87af14d097606da2a8e52ad59976ebd
Radman Avakoli <radman.avakoli@gravit-e.ca>
2023-08-06 17:24:30 -0700

    removed comments in ExpenseSplit"

--- pro-planner-client/src/components/ExpenseSplit.jsx
+++ pro-planner-client/src/components/ExpenseSplit.jsx
@@ -84,23 +84,6 @@ const ExpenseSplit = ({ className }) => {
     }
 
     processExpenses();
-    // useEffect(() => {
-    //     console.log("cost: " + cost)
-    //     if (cost) {
-    //         Object.values(cost).forEach((user) => {
-    //             const userName = user.userName;
-    //             const userInputs = Object.values(user.expenses);
-    //             const userExpenses = userInputs.reduce((total, expense) => total + expense.amount, 0);
-    //             userSpendings.push([userName, userExpenses])
-    //             totalSpendings += userExpenses;
-    //         });
-    
-    //         costPerUser =  parseFloat((totalSpendings / Object.keys(cost).length).toFixed(2));
-    //         spentToAvailableRatio = Math.round(totalSpendings / budget * 100);
-    //     }
-    // });
-
-    
 
     return (
         <>
